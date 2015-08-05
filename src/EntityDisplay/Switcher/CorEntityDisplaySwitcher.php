abbf6e081f6b9ea27814ffce7ca4a6b9c2051c06
Andreas Hennings <andreas@dqxtech.net>
2015-08-05 19:54:08 +0200

    Introduce Chain-of-responsibility display switcher

---
+++ src/EntityDisplay/Switcher/CorEntityDisplaySwitcher.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace Drupal\renderkit\EntityDisplay\Switcher;
+
+use Drupal\renderkit\EntityDisplay\EntityDisplayInterface;
+
+/**
+ * Chain-of-responsibility entity display switcher.
+ */
+class CorEntityDisplaySwitcher implements EntityDisplayInterface {
+
+  /**
+   * The displays to try.
+   *
+   * @var \Drupal\renderkit\EntityDisplay\EntityDisplayInterface[]
+   *   Format: $[] = $displayHandler
+   */
+  private $displays = array();
+
+  /**
+   * @param \Drupal\renderkit\EntityDisplay\EntityDisplayInterface[] $displays
+   */
+  function __construct(array $displays) {
+    $this->displays = $displays;
+  }
+
+  /**
+   * @param string $entity_type
+   * @param object[] $entities
+   *
+   * @return array[]
+   */
+  function buildMultiple($entity_type, array $entities) {
+    $builds = array_fill_keys(array_keys($entities), NULL);
+    foreach ($this->displays as $display) {
+      foreach ($display->buildMultiple($entity_type, $entities) as $delta => $build) {
+        if (!empty($build)) {
+          if (empty($builds[$delta])) {
+            $builds[$delta] = $build;
+          }
+          unset($entities[$delta]);
+        }
+      }
+    }
+    return array_filter($builds);
+  }
+
+}
