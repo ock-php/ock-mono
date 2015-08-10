0787cb55a11c674f578e1002b7d0845890c042fb
Andreas Hennings <andreas@dqxtech.net>
2015-08-10 03:40:56 +0200

    Switchers are not decorators.

--- src/EntityDisplay/Switcher/EntityTypeDisplaySwitcher.php
+++ src/EntityDisplay/Switcher/EntityTypeDisplaySwitcher.php
@@ -2,15 +2,18 @@
 
 namespace Drupal\renderkit\EntityDisplay\Switcher;
 
-use Drupal\renderkit\EntityDisplay\Decorator\NeutralEntityDisplayDecorator;
 use Drupal\renderkit\EntityDisplay\EntityDisplayInterface;
+use Drupal\renderkit\EntityDisplay\EntityDisplayMultipleBase;
 
 /**
  * Uses a different display handler depending on the entity type.
- *
- * The "decorated" display handler of the parent class is used as a fallback.
  */
-class EntityTypeDisplaySwitcher extends NeutralEntityDisplayDecorator {
+class EntityTypeDisplaySwitcher extends EntityDisplayMultipleBase {
+
+  /**
+   * @var \Drupal\renderkit\EntityDisplay\EntityDisplayInterface|null
+   */
+  private $fallbackDisplay;
 
   /**
    * The displays to use per entity type.
@@ -19,6 +22,19 @@ class EntityTypeDisplaySwitcher extends NeutralEntityDisplayDecorator {
    *   Format: $[$entityType] = $displayHandler
    */
   private $typeDisplays = array();
+
+  /**
+   * Sets a fallback entity display handler to use for entity types where no
+   * specific handler is configured.
+   *
+   * @param \Drupal\renderkit\EntityDisplay\EntityDisplayInterface $decorated
+   *
+   * @return $this
+   */
+  function setFallbackDisplay(EntityDisplayInterface $decorated) {
+    $this->fallbackDisplay = $decorated;
+    return $this;
+  }
 
   /**
    * Sets the display that will be used for entities of this type.
@@ -35,6 +51,8 @@ class EntityTypeDisplaySwitcher extends NeutralEntityDisplayDecorator {
   }
 
   /**
+   * Builds render arrays from the entities provided.
+   *
    * @param string $entity_type
    * @param object[] $entities
    *
@@ -43,8 +61,9 @@ class EntityTypeDisplaySwitcher extends NeutralEntityDisplayDecorator {
   function buildMultiple($entity_type, array $entities) {
     return isset($this->typeDisplays[$entity_type])
       ? $this->typeDisplays[$entity_type]->buildMultiple($entity_type, $entities)
-      // Use the "decorated" display as a fallback.
-      : parent::buildMultiple($entity_type, $entities);
+      : (isset($this->fallbackDisplay)
+        ? $this->fallbackDisplay->buildMultiple($entity_type, $entities)
+        : array());
   }
 
 }
