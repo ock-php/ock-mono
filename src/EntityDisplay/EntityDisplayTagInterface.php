d10307f9c256a3135b1f98c806dca4c554409f3a
Andreas Hennings <andreas@dqxtech.net>
2015-08-10 02:35:53 +0200

    Remove EntityDisplayTagInterface

--- src/EntityDisplay/EntityDisplayTagInterface.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace Drupal\renderkit\EntityDisplay;
-
-use Drupal\renderkit\Html\HtmlTagInterface;
-
-interface EntityDisplayTagInterface extends EntityDisplayAttributesInterface, HtmlTagInterface {
-
-}
