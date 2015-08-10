4b63d8c2ee45ab8db50ad2ffafc53c8e5c437843
Andreas Hennings <andreas@dqxtech.net>
2015-08-10 04:30:43 +0200

    Docblock

--- src/EntityDisplay/EntityDisplayInterface.php
+++ src/EntityDisplay/EntityDisplayInterface.php
@@ -8,6 +8,12 @@ namespace Drupal\renderkit\EntityDisplay;
  * An entity display handler builds render arrays from entities. Depending on
  * implementation, the render array could be for the whole entity, or just a
  * part of it (a title, a field, etc).
+ *
+ * Implementing classes should use one of the base classes, so they only need to
+ * implement one of the two methods, either buildEntities() or buildEntity().
+ *
+ * @see EntityDisplayBase
+ * @see EntitiesDisplaysBase
  */
 interface EntityDisplayInterface {
 
@@ -32,6 +38,8 @@ interface EntityDisplayInterface {
   function buildEntities($entity_type, array $entities);
 
   /**
+   * Same as ->buildEntities(), just for a single entity.
+   *
    * @param string $entity_type
    * @param object $entity
    *
