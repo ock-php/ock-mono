ad4ce4b33639e9de1e076cb17852185d874c0cd2
Andreas Hennings <andreas@dqxtech.net>
2016-04-07 15:00:44 +0200

    Add EntityToEntity_ChainOfTwo

---
+++ src/EntityToEntity/EntityToEntity_ChainOfTwo.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace Drupal\renderkit\EntityToEntity;
+
+class EntityToEntity_ChainOfTwo implements EntityToEntityInterface {
+
+  /**
+   * @var \Drupal\renderkit\EntityToEntity\EntityToEntityInterface
+   */
+  private $first;
+
+  /**
+   * @var \Drupal\renderkit\EntityToEntity\EntityToEntityInterface
+   */
+  private $second;
+
+  /**
+   * @param \Drupal\renderkit\EntityToEntity\EntityToEntityInterface $first
+   * @param \Drupal\renderkit\EntityToEntity\EntityToEntityInterface $second
+   */
+  function __construct(EntityToEntityInterface $first, EntityToEntityInterface $second) {
+    $this->first = $first;
+    $this->second = $second;
+  }
+
+  /**
+   * Gets the entity type of the referenced entities.
+   *
+   * @return string
+   */
+  function getTargetType() {
+    return $this->second->getTargetType();
+  }
+
+  /**
+   * @param string $entityType
+   * @param object[] $entities
+   *
+   * @return object[]
+   */
+  function entitiesGetRelated($entityType, array $entities) {
+    $related = $this->first->entitiesGetRelated($entityType, $entities);
+    return $this->second->entitiesGetRelated($this->first->getTargetType(), $related);
+  }
+
+  /**
+   * @param string $entityType
+   * @param object $entity
+   *
+   * @return object|null
+   */
+  function entityGetRelated($entityType, $entity) {
+    $related = $this->first->entityGetRelated($entityType, $entity);
+    if (NULL === $related) {
+      return NULL;
+    }
+    return $this->second->entityGetRelated($this->first->getTargetType(), $related);
+  }
+}
