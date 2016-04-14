fd3dacf8c478d0625ca206bdd0ee653deac9e7be
Andreas Hennings <andreas@dqxtech.net>
2016-04-14 23:24:22 +0200

    Add BuildProvider_Sequence

---
+++ src/BuildProvider/BuildProvider_Sequence.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace Drupal\renderkit\BuildProvider;
+
+use Drupal\cfrapi\Configurator\Sequence\Configurator_Sequence;
+use Drupal\cfrapi\Context\CfrContextInterface;
+use Drupal\cfrreflection\Configurator\Configurator_CallbackConfigurable;
+use Drupal\renderkit\ListFormat\ListFormatInterface;
+
+class BuildProvider_Sequence implements BuildProviderInterface {
+
+  /**
+   * @var \Drupal\renderkit\BuildProvider\BuildProviderInterface[]
+   */
+  private $providers;
+
+  /**
+   * @var \Drupal\renderkit\ListFormat\ListFormatInterface
+   */
+  private $listFormat;
+
+  /**
+   * @CfrPlugin(
+   *   id = "sequence",
+   *   label = "Sequence of build providers"
+   * )
+   *
+   * @param \Drupal\cfrapi\Context\CfrContextInterface $context
+   *
+   * @return \Drupal\cfrapi\Configurator\ConfiguratorInterface
+   */
+  static function createConfigurator(CfrContextInterface $context = NULL) {
+    return Configurator_CallbackConfigurable::createFromClassName(
+      __CLASS__,
+      array(
+        new Configurator_Sequence(
+          cfrplugin()->interfaceGetOptionalConfigurator(
+            BuildProviderInterface::class,
+            $context)
+        ),
+        cfrplugin()->interfaceGetOptionalConfigurator(
+          ListFormatInterface::class,
+          $context),
+      ),
+      array(
+        NULL,
+        t('List format'),
+      ));
+  }
+
+  /**
+   * @param \Drupal\renderkit\BuildProvider\BuildProviderInterface[] $providers
+   * @param \Drupal\renderkit\ListFormat\ListFormatInterface $listFormat
+   */
+  function __construct(array $providers, ListFormatInterface $listFormat = NULL) {
+    $this->providers = $providers;
+    $this->listFormat = $listFormat;
+  }
+
+  /**
+   * @return array
+   *   A render array.
+   */
+  function build() {
+    $builds = array();
+    foreach ($this->providers as $k => $provider) {
+      $build = $provider->build();
+      if (is_array($build) && array() !== $build) {
+        $builds[$k] = $provider->build();
+      }
+    }
+    if (array() === $builds) {
+      return array();
+    }
+    if (NULL !== $this->listFormat) {
+      $builds = $this->listFormat->buildList($builds);
+    }
+    return $builds;
+  }
+}
