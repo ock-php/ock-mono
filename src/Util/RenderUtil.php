bfce14ea57e2777ac4c3aa10a9530bdd06488652
Andreas Hennings <andreas@dqxtech.net>
2016-05-17 07:15:08 +0200

    Add RenderUtil class.

---
+++ src/Util/RenderUtil.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Drupal\renderkit\Util;
+
+final class RenderUtil extends UtilBase {
+
+  /**
+   * @param array $images
+   */
+  static function validateImages(array $images) {
+    self::validateList($images);
+    foreach ($images as $delta => $image) {
+      self::validateImage($image);
+    }
+  }
+
+  /**
+   * @param array $image
+   */
+  static function validateImage(array $image) {
+    if (!array_key_exists('#theme', $image)) {
+      throw new \RuntimeException("\$image['#theme'] is not set.");
+    }
+    if ('image' !== $image['#theme']) {
+      throw new \RuntimeException("\$image['#theme'] !== 'image'.");
+    }
+  }
+
+  /**
+   * @param array $items
+   */
+  static function validateList(array $items) {
+    foreach ($items as $delta => $item) {
+      if ('#' === $delta[0]) {
+        throw new \RuntimeException("Illegal delta '$delta'.");
+      }
+      if (!is_array($item)) {
+        $item_export = var_export($item, TRUE);
+        throw new \RuntimeException("Item at delta '$delta' is not an array, but $item_export.");
+      }
+    }
+  }
+
+}
