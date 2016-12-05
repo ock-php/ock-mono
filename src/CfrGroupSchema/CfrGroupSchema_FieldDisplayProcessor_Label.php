fac99ebd998df5405092e0066becf79cc20f896d
Andreas Hennings <andreas@dqxtech.net>
2016-12-05 10:48:24 +0100

    Add CfrGroupSchema_FieldDisplayProcessor_Label

---
+++ src/CfrGroupSchema/CfrGroupSchema_FieldDisplayProcessor_Label.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace Drupal\renderkit\CfrGroupSchema;
+
+use Drupal\cfrapi\CfrGroupSchema\CfrGroupSchema_DecoratorBase;
+use Drupal\cfrapi\Configurator\Bool\Configurator_Checkbox;
+use Drupal\cfrkit\Configurator\Configurator_Textfield;
+use Drupal\renderkit\FieldDisplayProcessor\FieldDisplayProcessor_Label;
+use Drupal\renderkit\FieldDisplayProcessor\FieldDisplayProcessorInterface;
+
+class CfrGroupSchema_FieldDisplayProcessor_Label extends CfrGroupSchema_DecoratorBase {
+
+  /**
+   * @return \Drupal\cfrapi\Configurator\ConfiguratorInterface[]
+   */
+  public function getConfigurators() {
+    $configurators = parent::getConfigurators();
+    $configurators['lb-col'] = new Configurator_Checkbox();
+    $configurators['label'] = new Configurator_Textfield(FALSE);
+    return $configurators;
+  }
+
+  /**
+   * @return string[]
+   */
+  public function getLabels() {
+    $labels = parent::getLabels();
+    $labels['lb-col'] = t('Hide label colon');
+    $labels['label'] = t('Label');
+    return $labels;
+  }
+
+  /**
+   * @param mixed[] $values
+   *   Values returned from group configurators.
+   *
+   * @return mixed
+   */
+  public function valuesGetValue(array $values) {
+
+    $fdp = parent::valuesGetValue($values);
+
+    if (!$fdp instanceof FieldDisplayProcessorInterface) {
+      return $fdp;
+    }
+
+    $fdp = new FieldDisplayProcessor_Label($fdp);
+
+    if ($values['lb-col']) {
+      $fdp = $fdp->withoutLabelColon();
+    }
+
+    if ('' !== $values['label']) {
+      $fdp = $fdp->withCustomLabel($values['label']);
+    }
+
+    return $fdp;
+  }
+
+}
