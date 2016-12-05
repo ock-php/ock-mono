4a3853a3a96b9f6ed959cff5044ef4f9c81dbf80
Andreas Hennings <andreas@dqxtech.net>
2016-12-05 10:42:08 +0100

    Add FieldDisplayProcessor_Bare.

---
+++ src/FieldDisplayProcessor/FieldDisplayProcessor_Bare.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Drupal\renderkit\FieldDisplayProcessor;
+
+class FieldDisplayProcessor_Bare implements FieldDisplayProcessorInterface {
+
+  /**
+   * @param array $element
+   *   Render array with ['#theme' => 'field', ..]
+   *
+   * @return array
+   */
+  public function process(array $element) {
+
+    $builds = [];
+    foreach ($element['#items'] as $delta => $item) {
+      if (!empty($element[$delta])) {
+        $builds[$delta] = $element[$delta];
+      }
+    }
+
+    return $builds;
+  }
+}
