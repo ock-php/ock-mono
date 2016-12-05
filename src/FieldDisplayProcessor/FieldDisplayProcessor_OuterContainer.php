5e3ff5cf1441da8ab6a5c3cb04988312eff9ea46
Andreas Hennings <andreas@dqxtech.net>
2016-12-05 10:44:53 +0100

    Add FieldDisplayProcessor_OuterContainer

---
+++ src/FieldDisplayProcessor/FieldDisplayProcessor_OuterContainer.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace Drupal\renderkit\FieldDisplayProcessor;
+
+class FieldDisplayProcessor_OuterContainer implements FieldDisplayProcessorInterface {
+
+  /**
+   * @var \Drupal\renderkit\FieldDisplayProcessor\FieldDisplayProcessorInterface
+   */
+  private $decorated;
+
+  /**
+   * @var string[]
+   */
+  private $classes = [];
+
+  /**
+   * @param \Drupal\renderkit\FieldDisplayProcessor\FieldDisplayProcessorInterface $decorated
+   */
+  public function __construct(FieldDisplayProcessorInterface $decorated) {
+    $this->decorated = $decorated;
+  }
+
+  /**
+   * @param string[] $classes
+   *
+   * @return static
+   */
+  public function withAdditionalClasses(array $classes) {
+    $clone = clone $this;
+    foreach ($classes as $class) {
+      $clone->classes[] = $class;
+    }
+    return $clone;
+  }
+
+  /**
+   * @param array $element
+   *   Render array with ['#theme' => 'field', ..]
+   *
+   * @return array
+   */
+  public function process(array $element) {
+
+    if ([] === $modified = $this->decorated->process($element)) {
+      return [];
+    }
+
+    $classes = [
+      'field',
+      'field-name-' . str_replace('_', '-', $element['#field_name']),
+    ];
+
+    foreach ($this->classes as $class) {
+      $classes[] = $class;
+    }
+
+    $modified = [
+      '#type' => 'container',
+      'inner' => $modified,
+    ];
+
+    if ([] !== $classes) {
+      $modified['#attributes']['class'] = $classes;
+    }
+
+    return $modified;
+  }
+}
