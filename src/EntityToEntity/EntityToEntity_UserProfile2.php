0d67399468de150409c2fc11ede0b87d72585abb
Andreas Hennings <andreas@dqxtech.net>
2017-03-03 14:16:23 -0300

    Make EntityToEntity_UserProfile2 plugin 'userProfile2' work for all entity types via EntityToEntity_EntityAuthor.

--- src/EntityToEntity/EntityToEntity_UserProfile2.php
+++ src/EntityToEntity/EntityToEntity_UserProfile2.php
@@ -13,52 +13,46 @@ class EntityToEntity_UserProfile2 extends EntityToEntityBase {
   private $profile2TypeName;
 
   /**
+   * @var \Drupal\renderkit\EntityToEntity\EntityToEntityInterface|null
+   */
+  private $entityToUser;
+
+  /**
    * @CfrPlugin(
    *   id = "userProfile2",
    *   label = "Profile2 from user or author"
    * )
    *
-   * @param string $entityType
-   *   Contextual parameter.
-   *
    * @return \Drupal\cfrapi\Configurator\ConfiguratorInterface
    */
-  public static function createConfigurator($entityType = NULL) {
-    return Configurator_CallbackConfigurable::createFromCallable(
-      function($profile2TypeName = NULL) use ($entityType) {
-        return self::create($entityType, $profile2TypeName);
-      },
+  public static function createConfigurator() {
+    return Configurator_CallbackConfigurable::createFromClassStaticMethod(
+      self::class,
+      'create',
       [new Configurator_EntityBundleName('profile2', FALSE)],
       [t('Profile type')]
     );
   }
 
   /**
-   * @param string|null $entityType
    * @param string|null $profile2TypeName
    *
    * @return \Drupal\renderkit\EntityToEntity\EntityToEntity_ChainOfTwo|\Drupal\renderkit\EntityToEntity\EntityToEntity_UserProfile2
    */
-  public static function create($entityType = NULL, $profile2TypeName = NULL) {
-    $userToProfile2 = new self($profile2TypeName);
-    if ('user' === $entityType) {
-      return $userToProfile2;
-    }
-    if (NULL === $entityType) {
-      $entityToUser = EntityToEntity_EntityAuthor::userEntityOrAuthor();
-    }
-    else {
-      $entityToUser = new EntityToEntity_EntityAuthor();
-    }
-    return new EntityToEntity_ChainOfTwo($entityToUser, $userToProfile2);
+  public static function create($profile2TypeName = NULL) {
+    return new self($profile2TypeName, new EntityToEntity_EntityAuthor());
   }
 
   /**
    * @param string|null $profile2TypeName
    *   The profile2 bundle name.
+   * @param \Drupal\renderkit\EntityToEntity\EntityToEntityInterface|null $entityToUser
    */
-  public function __construct($profile2TypeName = NULL) {
+  public function __construct($profile2TypeName = NULL, EntityToEntityInterface $entityToUser = NULL) {
     $this->profile2TypeName = $profile2TypeName;
+    if ('user' === $entityToUser->getTargetType()) {
+      $this->entityToUser = $entityToUser;
+    }
   }
 
   /**
@@ -77,13 +71,36 @@ class EntityToEntity_UserProfile2 extends EntityToEntityBase {
    * @return object|null
    */
   public function entityGetRelated($entityType, $entity) {
-    if ('user' !== $entityType) {
+
+    if (NULL === $user = $this->entityGetUser($entityType, $entity)) {
       return NULL;
     }
-    $profile_or_profiles = \profile2_load_by_user($entity, $this->profile2TypeName);
+
+    $profile_or_profiles = \profile2_load_by_user($user, $this->profile2TypeName);
+
     if (!is_array($profile_or_profiles)) {
       return $profile_or_profiles ?: NULL;
     }
+
     return reset($profile_or_profiles) ?: NULL;
+  }
+
+  /**
+   * @param string $entityType
+   * @param object $entity
+   *
+   * @return null|object
+   */
+  private function entityGetUser($entityType, $entity) {
+
+    if ('user' === $entityType) {
+      return $entity;
+    }
+
+    if (NULL !== $this->entityToUser) {
+      return $this->entityToUser->entityGetRelated($entityType, $entity);
+    }
+
+    return NULL;
   }
 }
