38aff931a04c5825f2e570ff3e6a53c655de8bb3
Andreas Hennings <andreas@dqxtech.net>
2017-08-10 03:51:53 +0200

    Fix obvious problems in BuildProcessor_ClassAttribute, as far as possible.

--- src/BuildProcessor/BuildProcessor_ClassAttribute.php
+++ src/BuildProcessor/BuildProcessor_ClassAttribute.php
@@ -2,6 +2,9 @@
 
 namespace Drupal\renderkit\BuildProcessor;
 
+/**
+ * This is currently unused and untested!
+ */
 class BuildProcessor_ClassAttribute implements BuildProcessorInterface {
 
   /**
@@ -31,17 +34,20 @@ class BuildProcessor_ClassAttribute implements BuildProcessorInterface {
    *   Render array after the processing.
    */
   public function process(array $build) {
+
     if (!isset($build[$this->attributesKey])) {
       return $build;
     }
+
     if (!isset($build[$this->attributesKey]['class'])) {
-      $this->attributesKey['class'] = $this->classes;
+      $build[$this->attributesKey]['class'] = $this->classes;
     }
     else {
       foreach ($this->classes as $class) {
-        $this->attributesKey['class'][] = $class;
+        $build[$this->attributesKey]['class'][] = $class;
       }
     }
+
     return $build;
   }
 }
