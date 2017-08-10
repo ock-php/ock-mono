a2bcf1379e09798dffa7167226532dfd9b241a81
Andreas Hennings <andreas@dqxtech.net>
2017-08-10 04:31:57 +0200

    D8: Simplify EntityDisplay_ViewModeBase using EntityDisplay_GroupByTypeBase.

--- src/EntityDisplay/EntityDisplay_ViewModeBase.php
+++ src/EntityDisplay/EntityDisplay_ViewModeBase.php
@@ -4,7 +4,7 @@ namespace Drupal\renderkit8\EntityDisplay;
 
 use Drupal\Core\Entity\EntityTypeManagerInterface;
 
-abstract class EntityDisplay_ViewModeBase extends EntitiesDisplayBase {
+abstract class EntityDisplay_ViewModeBase extends EntityDisplay_GroupByTypeBase {
 
   /**
    * @var \Drupal\Core\Entity\EntityTypeManagerInterface
@@ -19,42 +19,17 @@ abstract class EntityDisplay_ViewModeBase extends EntitiesDisplayBase {
   }
 
   /**
+   * @param string $entityTypeId
    * @param \Drupal\Core\Entity\EntityInterface[] $entities
    *
    * @return array[]
-   *
-   * @see entity_view()
-   * @see node_view_multiple()
    */
-  public function buildEntities(array $entities) {
+  protected function typeBuildEntities($entityTypeId, array $entities) {
 
-    if ([] === $entities) {
-      // In Drupal 7, entity_view() and even more so node_view_multiple() had
-      // problems with $entities === [].
-      // In Drupal 8? Dunno, let's not push our luck.
-      return [];
-    }
+    $builder = $this->entityTypeManager->getViewBuilder($entityTypeId);
+    $viewMode = $this->etGetViewMode($entityTypeId);
 
-    $entitiesGrouped = [];
-    foreach ($entities as $delta => $entity) {
-      $entitiesGrouped[$entity->getEntityTypeId()][$delta] = $entity;
-    }
-
-    $buildsUnsorted = [];
-    foreach ($entitiesGrouped as $entityTypeId => $typeEntities) {
-      $builder = $this->entityTypeManager->getViewBuilder($entityTypeId);
-      $viewMode = $this->etGetViewMode($entityTypeId);
-      $buildsUnsorted += $builder->viewMultiple($typeEntities, $viewMode);
-    }
-
-    $builds = [];
-    foreach ($entities as $delta => $entity) {
-      if (isset($buildsUnsorted[$delta])) {
-        $builds[$delta] = $buildsUnsorted[$delta];
-      }
-    }
-
-    return $builds;
+    return $builder->viewMultiple($entities, $viewMode);
   }
 
   /**
