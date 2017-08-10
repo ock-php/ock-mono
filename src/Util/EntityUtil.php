4462388905b574e35befeaa6dada3291a95affc9
Andreas Hennings <andreas@dqxtech.net>
2017-08-10 06:44:23 +0200

    D8: Change EntityUtil for Drupal 8.

--- src/Util/EntityUtil.php
+++ src/Util/EntityUtil.php
@@ -5,22 +5,98 @@ namespace Drupal\renderkit8\Util;
 final class EntityUtil extends UtilBase {
 
   /**
-   * @param string $entityType
+   * @param \Drupal\Core\Entity\EntityInterface[] $entities
+   *   Format: $[$delta] = $entity
+   *
+   * @return \Drupal\Core\Entity\EntityInterface[][]
+   *   Format: $[$type][$delta] = $entity
+   */
+  public static function entitiesGroupByType(array $entities) {
+
+    $grouped = [];
+    foreach ($entities as $delta => $entity) {
+      $grouped[$entity->getEntityTypeId()][$delta] = $entity;
+    }
+
+    return $grouped;
+  }
+
+  /**
+   * @param \Drupal\Core\Entity\EntityInterface[] $entities
+   *   Format: $[$delta] = $entity
+   * @param string $entityTypeId
+   *
+   * @return \Drupal\Core\Entity\EntityInterface[]
+   *   Format: $[$delta] = $entity
+   */
+  public static function entitiesFilterByType(array $entities, $entityTypeId) {
+
+    $filtered = [];
+    foreach ($entities as $delta => $entity) {
+      if ($entityTypeId === $entity->getEntityTypeId()) {
+        $filtered[$delta] = $entity;
+      }
+    }
+
+    return $filtered;
+  }
+
+  /**
+   * @param \Drupal\Core\Entity\EntityInterface[] $entities
+   *   Format: $[$delta] = $entity
+   * @param string $class
+   *
+   * @return \Drupal\Core\Entity\EntityInterface[]
+   *   Format: $[$delta] = $entity
+   */
+  public static function entitiesFilterByClass(array $entities, $class) {
+
+    $filtered = [];
+    foreach ($entities as $delta => $entity) {
+      if ($entity instanceof $class) {
+        $filtered[$delta] = $entity;
+      }
+    }
+
+    return $filtered;
+  }
+
+  /**
+   * @param \Drupal\Core\Entity\EntityInterface[] $entities
+   *   Format: $[$delta] = $entity
+   * @param string $class
+   * @param string $entityTypeId
+   *
+   * @return \Drupal\Core\Entity\EntityInterface[]
+   *   Format: $[$delta] = $entity
+   */
+  public static function entitiesFilterByClassAndType(array $entities, $class, $entityTypeId) {
+
+    $filtered = [];
+    foreach ($entities as $delta => $entity) {
+      if (1
+        && $entity instanceof $class
+        && $entityTypeId === $entity->getEntityTypeId()
+      ) {
+        $filtered[$delta] = $entity;
+      }
+    }
+
+    return $filtered;
+  }
+
+  /**
    * @param \Drupal\Core\Entity\EntityInterface[] $entities
    *
    * @return int[]
    */
-  public static function entitiesGetIds($entityType, $entities) {
-
-    $info = entity_get_info($entityType);
-    $idKey = $info['entity keys']['id'];
+  public static function entitiesGetIds($entities) {
 
     $idsByDelta = [];
     foreach ($entities as $delta => $entity) {
-      if (isset($entity->$idKey)) {
-        $idsByDelta[$delta] = $entity->$idKey;
-      }
+      $idsByDelta[$delta] = $entity->id();
     }
+
     return $idsByDelta;
   }
 
