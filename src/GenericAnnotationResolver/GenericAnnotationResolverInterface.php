21ca5ba28556fdccceb2731063e39f97c21a2ad9
Andreas Hennings <andreas@dqxtech.net>
2020-12-31 06:52:01 +0100

    STUB: Add GenericAnnotationResolverInterface.

---
+++ src/GenericAnnotationResolver/GenericAnnotationResolverInterface.php
@@ -0,0 +1,15 @@
+<?php
+
+interface GenericAnnotationResolverInterface {
+
+  /**
+   * @param string $name
+   *   The annotation tag name, without the "@".
+   * @param array $args
+   *   Values from the annotation body.
+   *
+   * @return mixed
+   */
+  public function resolveAnnotation(string $name, array $args);
+
+}
