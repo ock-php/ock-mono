00788fdeb21f6e6ab083038502d9257a17148277
Andreas Hennings <andreas@dqxtech.net>
2020-12-31 18:28:36 +0100

    (doc) Enhance docs.

--- src/Cache/CacheInterface.php
+++ src/Cache/CacheInterface.php
@@ -6,21 +6,33 @@ namespace Donquixote\Cf\Cache;
 interface CacheInterface {
 
   /**
+   * Reads a cache value into a variable.
+   *
    * @param string $key
+   *   Cache key.
    * @param mixed $value
+   *   (by reference) Variable that will contain the value.
    *
    * @return bool
+   *   TRUE if found, FALSE if not found.
    */
   public function getInto($key, &$value): bool;
 
   /**
+   * Writes a cache value.
+   *
    * @param string $key
+   *   Cache key.
    * @param mixed $value
+   *   Value to write.
    */
   public function set($key, $value): void;
 
   /**
+   * Clears cache entries.
+   *
    * @param string $prefix
+   *   (optional) Prefix for cache keys to be cleared.
    */
   public function clear($prefix = ''): void;
 
