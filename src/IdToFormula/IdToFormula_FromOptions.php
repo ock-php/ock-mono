d3f71a7378efd2ee77f7b861884b036977d62a95
Andreas Hennings <andreas@dqxtech.net>
2021-09-02 16:56:21 +0200

    AMEND delete many classes: Restore IdToFormula_FromOptions.

---
+++ src/IdToFormula/IdToFormula_FromOptions.php
@@ -0,0 +1,42 @@
+<?php
+declare(strict_types=1);
+
+namespace Donquixote\ObCK\IdToFormula;
+
+use Donquixote\ObCK\Core\Formula\FormulaInterface;
+use Donquixote\ObCK\Formula\Drilldown\Option\DrilldownOptionInterface;
+
+class IdToFormula_FromOptions implements IdToFormulaInterface {
+
+  /**
+   * @var \Donquixote\ObCK\Formula\Drilldown\Option\DrilldownOptionInterface[]
+   */
+  private $options;
+
+  /**
+   * Constructor.
+   *
+   * @param \Donquixote\ObCK\Formula\Drilldown\Option\DrilldownOptionInterface[] $options
+   */
+  public function __construct(array $options) {
+    self::validateOptions(...array_values($options));
+    $this->options = $options;
+  }
+
+  /**
+   * @param \Donquixote\ObCK\Formula\Drilldown\Option\DrilldownOptionInterface ...$options
+   */
+  private static function validateOptions(DrilldownOptionInterface ...$options): void {}
+
+  /**
+   * {@inheritdoc}
+   */
+  public function idGetFormula($id): ?FormulaInterface {
+
+    if (!isset($this->options[$id])) {
+      return NULL;
+    }
+
+    return $this->options[$id]->getFormula();
+  }
+}
