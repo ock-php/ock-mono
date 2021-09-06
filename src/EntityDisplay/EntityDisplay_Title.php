159463ff773e175659e6c5315df0f0070577c2f0
Andreas Hennings <andreas@dqxtech.net>
2021-09-06 14:35:57 +0200

    Fix and simplify formula for EntityDisplay_Title.

--- src/EntityDisplay/EntityDisplay_Title.php
+++ src/EntityDisplay/EntityDisplay_Title.php
@@ -4,8 +4,15 @@ declare(strict_types=1);
 namespace Drupal\renderkit\EntityDisplay;
 
 use Donquixote\ObCK\Core\Formula\FormulaInterface;
+use Donquixote\ObCK\Formula\Boolean\Formula_Boolean_YesNo;
+use Donquixote\ObCK\Formula\Formula;
+use Donquixote\ObCK\Text\Text;
 use Drupal\Core\Entity\EntityInterface;
-use Drupal\renderkit\Formula\Formula_EntityDisplay_Title;
+use Drupal\renderkit\BuildProcessor\BuildProcessor_Container;
+use Drupal\renderkit\EntityBuildProcessor\EntityBuildProcessor_Wrapper_LinkToEntity;
+use Drupal\renderkit\EntityDisplay\Decorator\EntityDisplay_WithBuildProcessor;
+use Drupal\renderkit\EntityDisplay\Decorator\EntityDisplay_WithEntityBuildProcessor;
+use Drupal\renderkit\Formula\Formula_TagName;
 
 /**
  * The most boring entity display handler, ever.
@@ -18,8 +25,7 @@ use Drupal\renderkit\Formula\Formula_EntityDisplay_Title;
 class EntityDisplay_Title extends EntityDisplayBase {
 
   /**
-   * Creates an "advanced" entity title plugin with optional link and wrapper
-   * tag name, e.g. <h2>.
+   * Advanced plugin formula with additional options.
    *
    * @CfrPlugin(
    *   id = "title",
@@ -29,13 +35,48 @@ class EntityDisplay_Title extends EntityDisplayBase {
    * @return \Donquixote\ObCK\Core\Formula\FormulaInterface
    */
   public static function createAdvancedFormula(): FormulaInterface {
-    return Formula_EntityDisplay_Title::create()->getValFormula();
+    return Formula::group()
+      ->add(
+        'tag_name',
+        Formula_TagName::createOptional(
+          ['h1', 'h2', 'h3', 'h4', 'strong']),
+        Text::t('Wrapper'))
+      ->add(
+        'link',
+        new Formula_Boolean_YesNo(),
+        Text::t('Link to entity'))
+      ->call([self::class, 'create']);
   }
 
   /**
-   * @param \Drupal\Core\Entity\EntityInterface $entity
+   * Static factory for the advanced formula.
    *
-   * @return array
+   * @param string|null $wrapperTagName
+   * @param bool $link
+   *
+   * @return \Drupal\renderkit\EntityDisplay\EntityDisplayInterface
+   */
+  public static function create(?string $wrapperTagName, bool $link): EntityDisplayInterface {
+
+    $display = new self();
+
+    if ($link) {
+      $display = new EntityDisplay_WithEntityBuildProcessor(
+        $display,
+        new EntityBuildProcessor_Wrapper_LinkToEntity());
+    }
+
+    if ($wrapperTagName) {
+      $display = new EntityDisplay_WithBuildProcessor(
+        $display,
+        BuildProcessor_Container::create($wrapperTagName));
+    }
+
+    return $display;
+  }
+
+  /**
+   * {@inheritdoc}
    */
   public function buildEntity(EntityInterface $entity): array {
     return [
