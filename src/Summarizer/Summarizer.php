812d428e1ab9f4e2d62cc3c7b4a0a78335eba024
Andreas Hennings <andreas@dqxtech.net>
2021-09-10 16:19:20 +0200

    Enhance Summarizer static methods.

--- src/Summarizer/Summarizer.php
+++ src/Summarizer/Summarizer.php
@@ -4,11 +4,29 @@ declare(strict_types=1);
 namespace Donquixote\ObCK\Summarizer;
 
 use Donquixote\ObCK\Core\Formula\FormulaInterface;
+use Donquixote\ObCK\Formula\Formula;
 use Donquixote\ObCK\Nursery\NurseryInterface;
 use Donquixote\ObCK\Util\MessageUtil;
 use Donquixote\ObCK\Util\UtilBase;
 
 final class Summarizer extends UtilBase {
+
+  /**
+   * @param string $interface
+   * @param \Donquixote\ObCK\Nursery\NurseryInterface $formulaToAnything
+   *
+   * @return \Donquixote\ObCK\Summarizer\SummarizerInterface
+   *
+   * @throws \Donquixote\ObCK\Exception\FormulaToAnythingException
+   */
+  public static function fromIface(
+    string $interface,
+    NurseryInterface $formulaToAnything
+  ): SummarizerInterface {
+    return self::fromFormula(
+      Formula::iface($interface),
+      $formulaToAnything);
+  }
 
   /**
    * @param \Donquixote\ObCK\Core\Formula\FormulaInterface $formula
@@ -24,21 +42,12 @@ final class Summarizer extends UtilBase {
     NurseryInterface $formulaToAnything
   ): SummarizerInterface {
 
+    /** @var \Donquixote\ObCK\Summarizer\SummarizerInterface $candidate */
     $candidate = $formulaToAnything->breed(
       $formula,
       SummarizerInterface::class);
 
-    if ($candidate instanceof SummarizerInterface) {
-      return $candidate;
-    }
-
-    throw new \RuntimeException(strtr(
-      'Misbehaving FTA for formula of class @formula_class: Expected @interface object, found @found.',
-      [
-        '@formula_class' => get_class($formula),
-        '@interface' => SummarizerInterface::class,
-        '@found' => MessageUtil::formatValue($candidate),
-      ]));
+    return $candidate;
   }
 
 }
