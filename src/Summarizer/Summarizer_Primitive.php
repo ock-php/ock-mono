24f72036279d824aeb102c79ddc19788a3c3bfd3
Andreas Hennings <andreas@dqxtech.net>
2021-09-10 16:17:22 +0200

    Add Summarizer_Primitive.

---
+++ src/Summarizer/Summarizer_Primitive.php
@@ -0,0 +1,44 @@
+<?php
+declare(strict_types=1);
+
+namespace Donquixote\ObCK\Summarizer;
+
+use Donquixote\ObCK\Formula\Primitive\Formula_PrimitiveInterface;
+use Donquixote\ObCK\Text\Text;
+use Donquixote\ObCK\Text\TextInterface;
+
+/**
+ * @STA
+ */
+class Summarizer_Primitive implements SummarizerInterface {
+
+  /**
+   * @var \Donquixote\ObCK\Formula\Primitive\Formula_PrimitiveInterface
+   */
+  private Formula_PrimitiveInterface $formula;
+
+  /**
+   * Constructor.
+   *
+   * @param \Donquixote\ObCK\Formula\Primitive\Formula_PrimitiveInterface $formula
+   */
+  public function __construct(Formula_PrimitiveInterface $formula) {
+    $this->formula = $formula;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function confGetSummary($conf): ?TextInterface {
+    $type = gettype($conf);
+    if (!in_array($type, $this->formula->getAllowedTypes())) {
+      return Text::t(
+        'Incompatible type: Expected @expected, found @found',
+        [
+          '@expected' => Text::s(implode('|', $this->formula->getAllowedTypes())),
+          '@found' => Text::s($type),
+        ]);
+    }
+    return Text::s(var_export($conf, TRUE));
+  }
+}
