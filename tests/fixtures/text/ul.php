be16cab63ca0e144ce1a8922ca55f55bed026d39
Andreas Hennings <andreas@dqxtech.net>
2021-09-11 01:19:54 +0200

    Enhance test cases for text.

--- tests/fixtures/text/ul.php
+++ tests/fixtures/text/ul.php
@@ -4,4 +4,6 @@ use Donquixote\ObCK\Text\Text;
 
 return Text::ul()
   ->add(Text::s('First item'))
-  ->add(Text::t('Second item'));
+  ->add(Text::t('Second item'))
+  ->addS('Third item')
+  ->addT('Fourth item');
