c903dfdff40997e6a635308e8dc9db1656812b47
Andreas Hennings <andreas@dqxtech.net>
2021-09-10 05:11:34 +0200

    Fix Formula_Drilldown_InlineExpanded.

--- src/Formula/Drilldown/Formula_Drilldown_InlineExpanded.php
+++ src/Formula/Drilldown/Formula_Drilldown_InlineExpanded.php
@@ -8,6 +8,7 @@ use Donquixote\ObCK\IdToFormula\IdToFormula_InlineExpanded;
 use Donquixote\ObCK\Formula\Id\Formula_IdInterface;
 use Donquixote\ObCK\Formula\Select\Formula_Select_InlineExpanded;
 use Donquixote\ObCK\Formula\Select\Formula_SelectInterface;
+use Donquixote\ObCK\Nursery\NurseryInterface;
 use Donquixote\ObCK\Util\UtilBase;
 
 final class Formula_Drilldown_InlineExpanded extends UtilBase {
@@ -15,10 +16,11 @@ final class Formula_Drilldown_InlineExpanded extends UtilBase {
   /**
    * @param \Donquixote\ObCK\Formula\Drilldown\Formula_DrilldownInterface $decorated
    * @param \Donquixote\ObCK\Formula\Id\Formula_IdInterface $idIsInline
+   * @param \Donquixote\ObCK\Nursery\NurseryInterface $nursery
    *
    * @return \Donquixote\ObCK\Formula\Drilldown\Formula_DrilldownInterface|null
    */
-  public static function create(Formula_DrilldownInterface $decorated, Formula_IdInterface $idIsInline): ?Formula_DrilldownInterface {
+  public static function create(Formula_DrilldownInterface $decorated, Formula_IdInterface $idIsInline, NurseryInterface $nursery): ?Formula_DrilldownInterface {
 
     $decoratedIdFormula = $decorated->getIdFormula();
 
@@ -30,10 +32,12 @@ final class Formula_Drilldown_InlineExpanded extends UtilBase {
       $decoratedIdFormula,
       new IdToFormula_FilterDecorator(
         $decorated->getIdToFormula(),
-        $idIsInline));
+        $idIsInline),
+      $nursery);
 
     $idToFormula = new IdToFormula_InlineExpanded(
-      $decorated->getIdToFormula());
+      $decorated->getIdToFormula(),
+      $nursery);
 
     return new Formula_Drilldown(
       $idFormula,
