17d03b8a4714da83658d6e4b0eae3f5503076db6
Andreas Hennings <andreas@dqxtech.net>
2021-09-13 03:49:33 +0200

    Add Optionlessness_Group.

---
+++ src/Optionlessness/Optionlessness_Group.php
@@ -0,0 +1,34 @@
+<?php
+declare(strict_types=1);
+
+namespace Donquixote\Ock\Optionlessness;
+
+use Donquixote\Ock\Formula\Group\Formula_GroupInterface;
+use Donquixote\Ock\Incarnator\IncarnatorInterface;
+use Donquixote\Ock\Util\UtilBase;
+
+/**
+ * Incarnator from Formula_Group* to Optionlessness*.
+ */
+final class Optionlessness_Group extends UtilBase {
+
+  /**
+   * @STA
+   *
+   * @param \Donquixote\Ock\Formula\Group\Formula_GroupInterface $group
+   * @param \Donquixote\Ock\Incarnator\IncarnatorInterface $incarnator
+   *
+   * @return \Donquixote\Ock\Optionlessness\OptionlessnessInterface
+   */
+  public static function fromFormula(Formula_GroupInterface $group, IncarnatorInterface $incarnator): OptionlessnessInterface {
+    foreach ($group->getItemFormulas() as $item) {
+      if (!Optionlessness::checkFormula($item, $incarnator)) {
+        // At least one group item has config options.
+        return new Optionlessness(FALSE);
+      }
+    }
+    // None of the group items are configurable.
+    return new Optionlessness(TRUE);
+  }
+
+}
