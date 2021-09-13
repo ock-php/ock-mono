62b51f83b2fb0b265d83461f2f44c7957c4c743c
Andreas Hennings <andreas@dqxtech.net>
2021-09-13 04:37:51 +0200

    Fix Formula_Fallback (to be removed later).

--- src/Formula/Fallback/Formula_Fallback.php
+++ src/Formula/Fallback/Formula_Fallback.php
@@ -4,12 +4,29 @@ declare(strict_types=1);
 
 namespace Donquixote\Ock\Formula\Fallback;
 
+use Donquixote\Ock\Core\Formula\FormulaInterface;
 use Donquixote\Ock\FormulaBase\Decorator\Formula_DecoratorBase;
 
 class Formula_Fallback extends Formula_DecoratorBase implements Formula_FallbackInterface {
 
+  private string $fallbackPhp;
+
+  /**
+   * Constructor.
+   *
+   * @param \Donquixote\Ock\Core\Formula\FormulaInterface $decorated
+   * @param string $fallback_php
+   */
+  public function __construct(FormulaInterface $decorated, string $fallback_php) {
+    parent::__construct($decorated);
+    $this->fallbackPhp = $fallback_php;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
   public function getFallbackPhp(): string {
-    // TODO: Implement getFallbackPhp() method.
+    return $this->fallbackPhp;
   }
 
 }
