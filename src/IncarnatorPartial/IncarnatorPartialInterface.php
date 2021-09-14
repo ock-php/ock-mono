98476cec3ce76dd6b4fc78f413d0417e9cfbe085
Andreas Hennings <andreas@dqxtech.net>
2021-09-14 04:19:28 +0200

    Docs in IncarnatorPartialInterface.

--- src/IncarnatorPartial/IncarnatorPartialInterface.php
+++ src/IncarnatorPartial/IncarnatorPartialInterface.php
@@ -7,23 +7,28 @@ use Donquixote\Ock\Core\Formula\FormulaInterface;
 use Donquixote\Ock\Incarnator\IncarnatorInterface;
 
 /**
+ * Partial incarnator, that supports specific types only.
+ *
  * @see \Donquixote\Ock\Incarnator\IncarnatorInterface
  */
 interface IncarnatorPartialInterface {
 
   /**
+   * Attempts to "incarnate" a formula as another type.
+   *
    * @param \Donquixote\Ock\Core\Formula\FormulaInterface $formula
-   *   Formula from which to breed a new object.
+   *   Formula from which to incarnate a new object.
    * @param string $interface
    *   Interface for the return value.
    * @param \Donquixote\Ock\Incarnator\IncarnatorInterface $incarnator
-   *   Nursery for nested breed calls.
+   *   Top-level incarnator that supports a wide range of formulas and
+   *   destination types.
    *
-   * @return null|object
+   * @return object|null
    *   An instance of $interface, or NULL to try other partials instead.
    *
    * @throws \Donquixote\Ock\Exception\IncarnatorException
-   *   Malfunction in a formula replacer.
+   *   Malfunction in an incarnator.
    */
   public function incarnate(
     FormulaInterface $formula,
