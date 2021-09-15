712bdc8447c00cbd9ef156d3f9cd725f4e85277f
Andreas Hennings <andreas@dqxtech.net>
2021-09-15 02:39:14 +0200

    Add doc comment.

--- src/Util/UtilBase.php
+++ src/Util/UtilBase.php
@@ -4,8 +4,16 @@ declare(strict_types=1);
 
 namespace Donquixote\Ock\Util;
 
+/**
+ * Base class for utility classes which shall only have static methods.
+ */
 abstract class UtilBase {
 
+  /**
+   * Private constructor.
+   *
+   * Prevents any subclass to be instantiated.
+   */
   final private function __construct() {}
 
 }
