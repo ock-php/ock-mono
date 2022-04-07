5c82fab17e1ad22fee17b66ae45fd213a0ba66c3
Andreas Hennings <andreas@dqxtech.net>
2022-04-07 02:40:54 +0200

    Add doc comment on exception.

--- src/Exception/MalformedAdapterDeclarationException.php
+++ src/Exception/MalformedAdapterDeclarationException.php
@@ -4,6 +4,9 @@ declare(strict_types=1);
 
 namespace Donquixote\Adaptism\Exception;
 
+/**
+ * An adapter declaration is malformed.
+ */
 class MalformedAdapterDeclarationException extends AdapterException {
 
 }
