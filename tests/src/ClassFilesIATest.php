0d24c5fb62eefea247b2cad73d55162ca1a78269
Andreas Hennings <andreas@dqxtech.net>
2024-05-09 18:39:16 +0200

    Clean up vertical space.

--- tests/src/ClassFilesIATest.php
+++ tests/src/ClassFilesIATest.php
@@ -18,34 +18,24 @@ class ClassFilesIATest extends TestCase {
    * @throws \ReflectionException
    */
   public function testClassFilesIA() {
-
     $classFilesIA = ClassFilesIA_NamespaceDirectoryPsr4::createFromClass(
       PlantInterface::class,
       1);
-
     $classFiles = iterator_to_array($classFilesIA->getIterator());
-
     asort($classFiles);
-
-    $this->assertSame(
-      [
-        __DIR__ . '/Fixtures/Acme/Animal/GreySquirrel.php' => GreySquirrel::class,
-        __DIR__ . '/Fixtures/Acme/Animal/RedSquirrel.php' => RedSquirrel::class,
-        __DIR__ . '/Fixtures/Acme/Plant/PlantInterface.php' => PlantInterface::class,
-        __DIR__ . '/Fixtures/Acme/Plant/Tree/Fig.php' => Fig::class,
-        __DIR__ . '/Fixtures/Acme/Plant/VenusFlyTrap.php' => VenusFlyTrap::class,
-      ],
-      $classFiles);
+    $this->assertSame([
+      __DIR__ . '/Fixtures/Acme/Animal/GreySquirrel.php' => GreySquirrel::class,
+      __DIR__ . '/Fixtures/Acme/Animal/RedSquirrel.php' => RedSquirrel::class,
+      __DIR__ . '/Fixtures/Acme/Plant/PlantInterface.php' => PlantInterface::class,
+      __DIR__ . '/Fixtures/Acme/Plant/Tree/Fig.php' => Fig::class,
+      __DIR__ . '/Fixtures/Acme/Plant/VenusFlyTrap.php' => VenusFlyTrap::class,
+    ], $classFiles);
   }
 
   public function testClassFilesIAEmpty() {
-
     $classFilesIA = new ClassFilesIA_Empty();
-
     $classFiles = iterator_to_array($classFilesIA->getIterator());
-
     asort($classFiles);
-
     $this->assertSame([], $classFiles);
   }
 
@@ -53,26 +43,19 @@ class ClassFilesIATest extends TestCase {
    * @throws \ReflectionException
    */
   public function testClassFilesIAMultiple() {
-
     $classFilesIAs = [];
     $classFilesIAs[] = ClassFilesIA_NamespaceDirectoryPsr4::createFromClass(
       Fig::class);
     $classFilesIAs[] = ClassFilesIA_NamespaceDirectoryPsr4::createFromClass(
       GreySquirrel::class);
-
     $classFilesIA = new ClassFilesIA_Multiple($classFilesIAs);
-
     $classFiles = iterator_to_array($classFilesIA->getIterator());
-
     asort($classFiles);
-
-    $this->assertSame(
-      [
-        __DIR__ . '/Fixtures/Acme/Animal/GreySquirrel.php' => GreySquirrel::class,
-        __DIR__ . '/Fixtures/Acme/Animal/RedSquirrel.php' => RedSquirrel::class,
-        __DIR__ . '/Fixtures/Acme/Plant/Tree/Fig.php' => Fig::class,
-      ],
-      $classFiles);
+    $this->assertSame([
+      __DIR__ . '/Fixtures/Acme/Animal/GreySquirrel.php' => GreySquirrel::class,
+      __DIR__ . '/Fixtures/Acme/Animal/RedSquirrel.php' => RedSquirrel::class,
+      __DIR__ . '/Fixtures/Acme/Plant/Tree/Fig.php' => Fig::class,
+    ], $classFiles);
   }
 
 }
