95d2d4942ef15f5f03869cc0590f1e8087991765
Andreas Hennings <andreas@dqxtech.net>
2024-05-09 15:55:26 +0200

    Let ReflectionClassesIA* return new ClassReflection object.

--- src/ReflectionClassesIA/ReflectionClassesIAInterface.php
+++ src/ReflectionClassesIA/ReflectionClassesIAInterface.php
@@ -3,12 +3,12 @@
 namespace Donquixote\ClassDiscovery\ReflectionClassesIA;
 
 /**
- * @template-implements \IteratorAggregate<mixed, \ReflectionClass>
+ * @template-implements \IteratorAggregate<mixed, \Donquixote\ClassDiscovery\Reflection\ClassReflection>
  */
 interface ReflectionClassesIAInterface extends \IteratorAggregate {
 
   /**
-   * @return \Iterator<int, \ReflectionClass>
+   * @return \Iterator<int, \Donquixote\ClassDiscovery\Reflection\ClassReflection>
    */
   public function getIterator(): \Iterator;
 
