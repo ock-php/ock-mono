c5284fc78d8b0553c5f07e376dd7e41577653431
Andreas Hennings <andreas@dqxtech.net>
2024-05-11 00:13:07 +0200

    Suppress phpstan for some catches.

--- packages/class-discovery/src/Reflection/ClassReflection.php
+++ packages/class-discovery/src/Reflection/ClassReflection.php
@@ -25,6 +25,7 @@ class ClassReflection extends \ReflectionClass implements FactoryReflectionInter
     try {
       parent::__construct($objectOrClass);
     }
+    // @phpstan-ignore-next-line
     catch (\Error $e) {
       assert(is_string($objectOrClass));
       throw new \ReflectionException(sprintf(
