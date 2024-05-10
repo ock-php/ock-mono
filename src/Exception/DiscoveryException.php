2f9e67ed4fa6354404ab4263532e7cff823bec49
Andreas Hennings <andreas@dqxtech.net>
2024-05-10 02:21:24 +0200

    Improve docs.

--- src/Exception/DiscoveryException.php
+++ src/Exception/DiscoveryException.php
@@ -5,7 +5,7 @@ declare(strict_types = 1);
 namespace Donquixote\DID\Exception;
 
 /**
- * A declaration, typically one with attributes, is malformed.
+ * Something went wrong during discovery.
  */
 class DiscoveryException extends \Exception {
 
