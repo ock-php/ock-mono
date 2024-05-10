ed07ee00cb53d5890f8af04d1310b39c62fcb5ad
Andreas Hennings <andreas@dqxtech.net>
2024-05-10 21:20:59 +0200

    Add ClassNamesIA_Array.

---
+++ packages/class-discovery/src/ClassNamesIA/ClassNamesIA_Array.php
@@ -0,0 +1,36 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Donquixote\ClassDiscovery\ClassNamesIA;
+
+/**
+ * @template TKey
+ *
+ * @template-implements ClassNamesIAInterface<TKey>
+ */
+class ClassNamesIA_Array implements ClassNamesIAInterface {
+
+  /**
+   * Constructor.
+   *
+   * @param array<TKey, class-string> $values
+   */
+  public function __construct(
+    private readonly array $values,
+  ) {
+    // Validate input if assertions are enabled.
+    assert(!array_filter(
+      $values,
+      fn ($value) => !is_string($value),
+    ), 'Array values must be class names.');
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getIterator(): \Iterator {
+    yield from $this->values;
+  }
+
+}
