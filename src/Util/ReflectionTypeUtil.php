6e7b23a5f7d512565c2fc8f787eb70f8ea95744b
Andreas Hennings <andreas@dqxtech.net>
2024-05-10 03:19:27 +0200

    Support FactoryReflectionInterface in ReflectionTypeUtil, and refactor.

--- src/Util/ReflectionTypeUtil.php
+++ src/Util/ReflectionTypeUtil.php
@@ -3,86 +3,106 @@ declare(strict_types=1);
 
 namespace Donquixote\DID\Util;
 
+use Donquixote\ClassDiscovery\Reflection\FactoryReflectionInterface;
 use Donquixote\CodegenTools\Util\MessageUtil;
 use Donquixote\DID\Exception\MalformedDeclarationException;
 
 class ReflectionTypeUtil {
 
   /**
-   * @param \ReflectionParameter|\ReflectionFunctionAbstract $reflector
+   * @param \ReflectionParameter|\ReflectionFunctionAbstract|\ReflectionClass|FactoryReflectionInterface $reflector
    * @param bool $allowObject
    *   TRUE, to allow 'object' return type.
    *
    * @return class-string|null
    * @psalm-return ($allowObject is true ? (class-string|null) : class-string)
+   * @phpstan-return ($allowObject is true ? (class-string|null) : class-string)
    *   The type, or NULL for 'object'.
    *
    * @throws \Donquixote\DID\Exception\MalformedDeclarationException
+   *   The return type does not match the expectation.
    */
   public static function requireGetClassLikeType(
-    \ReflectionParameter|\ReflectionFunctionAbstract $reflector,
+    \ReflectionParameter|\ReflectionFunctionAbstract|\ReflectionClass|FactoryReflectionInterface $reflector,
     bool $allowObject = false,
   ): ?string {
-    $type = $reflector instanceof \ReflectionParameter
-      ? $reflector->getType()
-      : $reflector->getReturnType();
-    if (!$type instanceof \ReflectionNamedType || $type->isBuiltin()) {
-      if ($allowObject && $type instanceof \ReflectionNamedType && $type->getName() === 'object') {
+    $type = self::readType($reflector);
+    if ($type !== false) {
+      if ($type !== true) {
+        return $type;
+      }
+      if ($allowObject) {
         return null;
       }
-      throw new MalformedDeclarationException(\sprintf(
-        'Expected a class-like %s declaration on %s.',
-        $reflector instanceof \ReflectionParameter ? 'type' : 'return type',
-        MessageUtil::formatReflector($reflector),
-      ));
     }
-    $name = $type->getName();
-    if ($name !== 'self' && $name !== 'static') {
-      return $name;
-    }
-    $reflectionFunction = $reflector instanceof \ReflectionParameter
-      ? $reflector->getDeclaringFunction()
-      : $reflector;
-    if (!$reflectionFunction instanceof \ReflectionMethod) {
-      throw new MalformedDeclarationException(\sprintf(
-        'Unexpected %s outside class context, in type declaration for %s.',
-        "'$name'",
-        MessageUtil::formatReflector($reflector),
-      ));
-    }
-    return $reflectionFunction->getDeclaringClass()->getName();
+    throw new MalformedDeclarationException(\sprintf(
+      'Expected a class-like %s declaration on %s.',
+      $reflector instanceof \ReflectionParameter ? 'type' : 'return type',
+      MessageUtil::formatReflector($reflector),
+    ));
   }
 
   /**
-   * @param \ReflectionParameter|\ReflectionFunctionAbstract $reflector
+   * @param \ReflectionParameter|\ReflectionFunctionAbstract|\ReflectionClass|FactoryReflectionInterface $reflector
    *
    * @return class-string|null
-   * @throws \Donquixote\DID\Exception\MalformedDeclarationException
    */
   public static function getClassLikeType(
-    \ReflectionParameter|\ReflectionFunctionAbstract $reflector,
+    \ReflectionParameter|\ReflectionFunctionAbstract|\ReflectionClass|FactoryReflectionInterface $reflector,
   ): ?string {
-    $type = $reflector instanceof \ReflectionParameter
-      ? $reflector->getType()
-      : $reflector->getReturnType();
-    if (!$type instanceof \ReflectionNamedType || $type->isBuiltin()) {
-      return null;
+    $type = self::readType($reflector);
+    return match ($type) {
+      true, false => null,
+      default => $type,
+    };
+  }
+
+  /**
+   * Gets the return value class name, if it is unique.
+   *
+   * @return class-string|bool
+   *   If this is a class, the class name is returned.
+   *   If this is a method:
+   *     - A class name, if the declared return type is a single class name.
+   *     - TRUE, if the single declared return type is 'object'.
+   *     - FALSE, if the declared type is not a single class name.
+   */
+  protected static function readType(
+    \ReflectionParameter|\ReflectionFunctionAbstract|\ReflectionClass|FactoryReflectionInterface $reflector,
+  ): string|bool {
+    if ($reflector instanceof \ReflectionClass) {
+      return $reflector->getName();
+    }
+    if ($reflector instanceof \ReflectionParameter) {
+      $type = $reflector->getType();
+    }
+    else {
+      $type = $reflector->getReturnType();
+    }
+    if (!$type instanceof \ReflectionNamedType) {
+      return false;
     }
     $name = $type->getName();
-    if ($name !== 'self' && $name !== 'static') {
-      return $name;
+    if ($type->isBuiltin()) {
+      if ($name === 'object') {
+        return true;
+      }
+      return false;
     }
-    $reflectionFunction = $reflector instanceof \ReflectionParameter
-      ? $reflector->getDeclaringFunction()
-      : $reflector;
-    if (!$reflectionFunction instanceof \ReflectionMethod) {
-      throw new MalformedDeclarationException(\sprintf(
-        'Unexpected %s outside class context, in type declaration for %s.',
-        "'$name'",
-        MessageUtil::formatReflector($reflector),
-      ));
+    if ($name === 'self' || $name === 'static') {
+      $reflectionFunction = $reflector instanceof \ReflectionParameter
+        ? $reflector->getDeclaringFunction()
+        : $reflector;
+      if (!$reflectionFunction instanceof \ReflectionMethod) {
+        throw new \RuntimeException(\sprintf(
+          'Unexpected %s outside class context, in type declaration for %s.',
+          "'$name'",
+          MessageUtil::formatReflector($reflector),
+        ));
+      }
+      $name = $reflectionFunction->getDeclaringClass()->getName();
     }
-    return $reflectionFunction->getDeclaringClass()->getName();
+    return $name;
   }
 
   /**
