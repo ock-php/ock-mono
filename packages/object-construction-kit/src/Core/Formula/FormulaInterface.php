8675f9fa96d0bfad5a0e76a7e1dc0266861ae365
Andreas Hennings <andreas@dqxtech.net>
2024-05-17 16:41:56 +0200

    Add docs.

--- packages/object-construction-kit/src/Core/Formula/FormulaInterface.php
+++ packages/object-construction-kit/src/Core/Formula/FormulaInterface.php
@@ -8,6 +8,15 @@ use Donquixote\Ock\Core\Formula\Base\FormulaBaseInterface;
 
 /**
  * This is a declarative interface only. It has no methods.
+ *
+ * A formula defines available configuration options for a plugin, and how an
+ * object or value can be generated from a given configuration value.
+ *
+ * The actual behavior for each formula type has to be implemented in adapter
+ * classes.
+ *
+ * This architecture allows to add new behavior to a formula type, without
+ * adding new methods to the formula interface.
  */
 interface FormulaInterface extends FormulaBaseInterface {
 
