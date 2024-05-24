05771bc9fdb0097685432ec08a4b8c4c058d398a
Andreas Hennings <andreas@dqxtech.net>
2024-05-25 00:13:57 +0200

    Move service definitions back into adaptism/services.php.

--- packages/adaptism/services.php
+++ packages/adaptism/services.php
@@ -3,8 +3,34 @@
 declare(strict_types = 1);
 
 use Ock\Adaptism\AdaptismPackage;
+use Ock\ClassDiscovery\Discovery\DiscoveryInterface;
+use Ock\ClassDiscovery\Discovery\FactoryDiscovery;
+use Ock\Egg\ClassToEgg\ClassToEgg_Construct;
+use Ock\Egg\ClassToEgg\ClassToEggInterface;
+use Ock\Egg\ParamToEgg\ParamToEgg_Chain;
+use Ock\Egg\ParamToEgg\ParamToEggInterface;
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
+use function Symfony\Component\DependencyInjection\Loader\Configurator\tagged_iterator;
 
 return function (ContainerConfigurator $container): void {
-  AdaptismPackage::configureServices($container);
+  $services = $container->services();
+
+  $services->defaults()
+    ->autowire()
+    ->autoconfigure();
+
+  // The path is relative to the package root where 'services.php' is located.
+  $services->load('Ock\\Adaptism\\', 'src/');
+
+  $services->set(DiscoveryInterface::class . ' $' . AdaptismPackage::DISCOVERY_TARGET)
+    ->public()
+    ->class(FactoryDiscovery::class)
+    ->factory([FactoryDiscovery::class, 'fromCandidateObjects'])
+    ->arg(0, tagged_iterator(AdaptismPackage::DISCOVERY_TAG_NAME));
+
+  $services->set(ClassToEggInterface::class)
+    ->class(ClassToEgg_Construct::class);
+
+  $services->set(ParamToEggInterface::class)
+    ->class(ParamToEgg_Chain::class);
 };
