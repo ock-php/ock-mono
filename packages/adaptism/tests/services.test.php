782383338b93539cba9e8983215b4d1d4d69a35f
Andreas Hennings <andreas@dqxtech.net>
2024-05-25 00:18:04 +0200

    Move service definitions back into adaptism/tests/services.test.php.

--- packages/adaptism/tests/services.test.php
+++ packages/adaptism/tests/services.test.php
@@ -6,9 +6,19 @@
 
 declare(strict_types=1);
 
-use Ock\Adaptism\Tests\Fixtures\AdaptismTestPackage;
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 
 return function (ContainerConfigurator $container): void {
-  AdaptismTestPackage::configureServices($container);
+  $services = $container->services();
+
+  $services->defaults()
+    ->autowire()
+    ->autoconfigure();
+
+  $services->load('Ock\\Adaptism\\Tests\\Fixtures\\', 'src/Fixtures/');
+
+  $services->set(\DateTimeZone::class)
+    ->class(\DateTimeZone::class)
+    ->public()
+    ->arg(0, 'America/New_York');
 };
