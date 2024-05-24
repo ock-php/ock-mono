fea7ce5ed961b984326fd66389e40c66eddaa97b
Andreas Hennings <andreas@dqxtech.net>
2024-05-25 00:21:01 +0200

    fixup! Move service definitions back into ock/services.php.

--- packages/object-construction-kit/src/OckPackage.php
+++ packages/object-construction-kit/src/OckPackage.php
@@ -4,16 +4,6 @@ declare(strict_types=1);
 
 namespace Ock\Ock;
 
-use Ock\ClassDiscovery\Discovery\DiscoveryInterface;
-use Ock\ClassDiscovery\Discovery\FactoryDiscovery;
-use Ock\ClassDiscovery\Inspector\FactoryInspector_Concat;
-use Ock\ClassDiscovery\Inspector\FactoryInspectorInterface;
-use Ock\ClassDiscovery\ReflectionClassesIA\ReflectionClassesIA_Concat;
-use Ock\ClassDiscovery\ReflectionClassesIA\ReflectionClassesIAInterface;
-use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
-use Symfony\Component\DependencyInjection\Reference;
-use function Symfony\Component\DependencyInjection\Loader\Configurator\tagged_iterator;
-
 class OckPackage {
 
   const NAMESPACE = __NAMESPACE__;
