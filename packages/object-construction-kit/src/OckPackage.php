ad0d1b2e44b19677a4f47b5592d8128a31d555be
Andreas Hennings <andreas@dqxtech.net>
2024-05-25 00:11:18 +0200

    Move service definitions back into ock/services.php.

--- packages/object-construction-kit/src/OckPackage.php
+++ packages/object-construction-kit/src/OckPackage.php
@@ -4,55 +4,14 @@ declare(strict_types=1);
 
 namespace Ock\Ock;
 
-use Ock\ClassDiscovery\Discovery\DiscoveryInterface;
-use Ock\ClassDiscovery\Discovery\FactoryDiscovery;
-use Ock\ClassDiscovery\Inspector\FactoryInspector_Concat;
-use Ock\ClassDiscovery\Inspector\FactoryInspectorInterface;
-use Ock\ClassDiscovery\ReflectionClassesIA\ReflectionClassesIA_Concat;
-use Ock\ClassDiscovery\ReflectionClassesIA\ReflectionClassesIAInterface;
-use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
-use Symfony\Component\DependencyInjection\Reference;
-use function Symfony\Component\DependencyInjection\Loader\Configurator\tagged_iterator;
-
 class OckPackage {
+
+  const NAMESPACE = __NAMESPACE__;
 
   const DIR = __DIR__;
 
   const DISCOVERY_TARGET = 'ockDiscovery';
 
   const DISCOVERY_TAG_NAME = 'ock.discovery';
-
-  /**
-   * Configures a symfony container.
-   *
-   * @param \Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator $container
-   */
-  public static function configureServices(ContainerConfigurator $container): void {
-    $services = $container->services();
-
-    $services->defaults()
-      ->autowire()
-      ->autoconfigure();
-
-    // The path is relative to the package root where 'services.php' is located.
-    $services->load(__NAMESPACE__ . '\\', 'src/');
-
-    $discoveryClassesServiceId = ReflectionClassesIAInterface::class . ' $' . self::DISCOVERY_TARGET;
-    $services->set($discoveryClassesServiceId)
-      ->class(ReflectionClassesIA_Concat::class)
-      ->factory([ReflectionClassesIA_Concat::class, 'fromCandidateObjects'])
-      ->arg(0, tagged_iterator(self::DISCOVERY_TAG_NAME));
-
-    $discoveryInspectorServiceId = FactoryInspectorInterface::class . ' $' . self::DISCOVERY_TARGET;
-    $services->set($discoveryInspectorServiceId)
-      ->class(FactoryInspector_Concat::class)
-      ->factory([FactoryInspector_Concat::class, 'fromCandidateObjects'])
-      ->arg(0, tagged_iterator(self::DISCOVERY_TAG_NAME));
-
-    $services->set(DiscoveryInterface::class . ' $' . self::DISCOVERY_TARGET)
-      ->class(FactoryDiscovery::class)
-      ->arg(0, new Reference($discoveryClassesServiceId))
-      ->arg(1, new Reference($discoveryInspectorServiceId));
-  }
 
 }
