75a5b632120e4e75a464267269c5027304c5c694
Andreas Hennings <andreas@dqxtech.net>
2024-05-28 01:52:49 +0200

    fixup! Scan ock_example module dir for ock plugins.

--- modules/ock/modules/ock_example/src/OckExampleNamespace.php
+++ modules/ock/modules/ock_example/src/OckExampleNamespace.php
@@ -9,7 +9,7 @@ use Ock\Ock\OckPackage;
 use Symfony\Component\DependencyInjection\Attribute\AutoconfigureTag;
 
 /**
- * Classes to scan for adapter definitions.
+ * Namespace to scan for ock plugins.
  */
 #[AutoconfigureTag(OckPackage::DISCOVERY_TAG_NAME)]
 class OckExampleNamespace extends CurrentNamespaceBase {
