0b2035aaba697849502158f822791914cdf0abae
Andreas Hennings <andreas@dqxtech.net>
2024-05-28 00:52:08 +0200

    Use symfony DI attributes.

--- modules/ock/src/Translator/Translator_Drupal.php
+++ modules/ock/src/Translator/Translator_Drupal.php
@@ -5,12 +5,11 @@ declare(strict_types=1);
 namespace Drupal\ock\Translator;
 
 use Drupal\Core\StringTranslation\TranslationInterface;
-use Ock\DID\Attribute\Parameter\GetService;
-use Ock\DID\Attribute\Service;
 use Ock\Ock\Translator\TranslatorInterface;
+use Symfony\Component\DependencyInjection\Attribute\AsAlias;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 
-#[Service]
+#[AsAlias(public: true)]
 class Translator_Drupal implements TranslatorInterface {
 
   /**
@@ -39,7 +38,6 @@ class Translator_Drupal implements TranslatorInterface {
    *   String translation service.
    */
   public function __construct(
-    #[GetService('string_translation')]
     private readonly TranslationInterface $translation,
   ) {}
 
