492453c938b8948825bcc31e03d3eae62a359f96
Andreas Hennings <andreas@dqxtech.net>
2024-05-28 01:42:39 +0200

    Discover services in renderkit module.

--- modules/renderkit/src/RenderkitServiceProvider.php
+++ modules/renderkit/src/RenderkitServiceProvider.php
@@ -3,26 +3,11 @@ declare(strict_types=1);
 
 namespace Drupal\renderkit;
 
-use Drupal\Core\DependencyInjection\ContainerBuilder;
-use Drupal\Core\DependencyInjection\ServiceProviderInterface;
-use Drupal\ock\DI\ServiceProvider\ServiceProvider_AttributesDiscovery;
-use Ock\ClassDiscovery\ClassFilesIA\ClassFilesIA;
+use Drupal\ock\OckServiceProviderBase;
 
 /**
- * Class with static methods for easier construction of display handlers.
+ * Service provider for 'renderkit' module.
  */
-class RenderkitServiceProvider implements ServiceProviderInterface {
-
-  /**
-   * {@inheritdoc}
-   *
-   * @throws \Exception
-   *   Malformed service declaration.
-   */
-  public function register(ContainerBuilder $container): void {
-    ServiceProvider_AttributesDiscovery::create()
-      ->withClassFilesIA(ClassFilesIA::psr4FromClass(self::class))
-      ->register($container);
-  }
+class RenderkitServiceProvider extends OckServiceProviderBase {
 
 }
