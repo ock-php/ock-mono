75fbe83f3241b3e508bd7b012d9da0854853344d
Andreas Hennings <andreas@dqxtech.net>
2024-05-28 01:38:43 +0200

    Add service provider for ock_example module.

---
+++ modules/ock/modules/ock_example/src/OckExampleServiceProvider.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace Drupal\ock_example;
+
+use Drupal\ock\OckServiceProviderBase;
+
+/**
+ * Service provider for the 'ock_example' module.
+ */
+class OckExampleServiceProvider extends OckServiceProviderBase {
+
+}
