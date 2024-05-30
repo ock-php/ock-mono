d5714e55df15b6b0c0e3f6b52580690902a76109
Andreas Hennings <andreas@dqxtech.net>
2024-05-30 15:44:03 +0200

    Themekit: Introduce ExceptionCleaner to avoid serialization issue in ThemekitElementTypesTest.

---
+++ modules/themekit/tests/src/Traits/ExceptionSerializationTrait.php
@@ -0,0 +1,32 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Drupal\Tests\themekit\Traits;
+
+use Drupal\Tests\themekit\Tools\ExceptionCleaner;
+
+/**
+ * Trait that cleans up exceptions and test failures, to make them serializable.
+ *
+ * This is relevant for tests with process isolation, if a backtrace contains
+ * values that cannot be serialized, such as closures, reflectors etc.
+ *
+ * Actually most of the exceptions in PhpUnit _are_ serializable. They have
+ * __sleep() methods that prevent the stack trace from being serialized.
+ *
+ * However, there is at least one exception where this is not the case.
+ */
+trait ExceptionSerializationTrait {
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function onNotSuccessfulTest(\Throwable $t): void {
+    $cleaner = new ExceptionCleaner();
+    $cleaner->cleanException($t);
+    /* @see \PHPUnit\Framework\TestCase::onNotSuccessfulTest() */
+    parent::onNotSuccessfulTest($t);
+  }
+
+}
