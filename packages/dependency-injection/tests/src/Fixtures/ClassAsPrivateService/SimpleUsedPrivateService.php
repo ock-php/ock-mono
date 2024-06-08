1eb0c1525014bd7c53ab3dfa0d434c6096e8f54a
Andreas Hennings <andreas@dqxtech.net>
2024-06-08 16:24:11 +0200

    Add mechanism to discover classes as private services.

---
+++ packages/dependency-injection/tests/src/Fixtures/ClassAsPrivateService/SimpleUsedPrivateService.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Ock\DependencyInjection\Tests\Fixtures\ClassAsPrivateService;
+
+/**
+ * A simple class that will be registered as private service, then forgotten.
+ */
+class SimpleUsedPrivateService {
+
+}
