24cd60d71b6072ba34f4028450b86a71e59d58da
Andreas Hennings <andreas@dqxtech.net>
2024-06-09 01:41:02 +0200

    Add inspector factories that support decorators.

---
+++ packages/class-discovery/src/Inspector/ClassInspector.php
@@ -0,0 +1,75 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Ock\ClassDiscovery\Inspector;
+
+/**
+ * Static factories for to create a class inspector.
+ */
+class ClassInspector {
+
+  /**
+   * Creates a new instance from a list of inspector candidates.
+   *
+   * @param array $candidates
+   *   List of objects that may or may not be inspectors.
+   *   This accepts any iterable, to support symfony tagged services.
+   * @param bool $includeFactoryInspectors
+   *   TRUE to also include factory inspectors.
+   *
+   * @return \Ock\ClassDiscovery\Inspector\ClassInspectorInterface
+   *   New instance.
+   */
+  public static function fromCandidateObjects(iterable $candidates, bool $includeFactoryInspectors): ClassInspectorInterface {
+    $classInspector = ClassInspector_Concat::fromCandidateObjects($candidates, false);
+    $classInspector = self::applyDecorators($classInspector, $candidates);
+    if ($includeFactoryInspectors) {
+      $factoryInspector = FactoryInspector::fromCandidateObjects($candidates);
+      if (!$factoryInspector instanceof FactoryInspector_Concat
+        || !$factoryInspector->isEmpty()
+      ) {
+        $classInspector = new ClassInspector_Concat([
+          $classInspector,
+          new ClassInspector_Factories($factoryInspector),
+        ]);
+      }
+    }
+    return $classInspector;
+  }
+
+  /**
+   * @param \Ock\ClassDiscovery\Inspector\ClassInspectorInterface $decorated
+   * @param iterable $candidates
+   *
+   * @return \Ock\ClassDiscovery\Inspector\ClassInspectorInterface
+   */
+  public static function applyDecorators(ClassInspectorInterface $decorated, iterable $candidates): ClassInspectorInterface {
+    foreach ($candidates as $candidate) {
+      if (!$candidate instanceof \Closure) {
+        continue;
+      }
+      $rf = new \ReflectionFunction($candidate);
+      $params = $rf->getParameters();
+      if (count($params) !== 1) {
+        continue;
+      }
+      $type = $params[0]->getType();
+      if (!$type) {
+        continue;
+      }
+      // See https://youtrack.jetbrains.com/issue/WI-77852/ReflectionType-toString-no-longer-deprecated
+      // @phpstan-ignore-next-line
+      if ($type->__toString() !== ClassInspectorInterface::class) {
+        continue;
+      }
+      $decorator = $candidate($decorated);
+      if (!$decorator instanceof ClassInspectorInterface) {
+        continue;
+      }
+      $decorated = $decorator;
+    }
+    return $decorated;
+  }
+
+}
---
+++ packages/class-discovery/src/Inspector/FactoryInspector.php
@@ -0,0 +1,63 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Ock\ClassDiscovery\Inspector;
+
+/**
+ * Static factories for to create a class inspector.
+ */
+class FactoryInspector {
+
+  /**
+   * Creates a new instance from a list of inspector candidates.
+   *
+   * @param array $candidates
+   *   List of objects that may or may not be inspectors.
+   *   This accepts any iterable, to support symfony tagged services.
+   *
+   * @return \Ock\ClassDiscovery\Inspector\FactoryInspectorInterface
+   *   New instance.
+   */
+  public static function fromCandidateObjects(iterable $candidates): FactoryInspectorInterface {
+    $classInspector = FactoryInspector_Concat::fromCandidateObjects($candidates);
+    $classInspector = self::applyDecorators($classInspector, $candidates);
+    return $classInspector;
+  }
+
+  /**
+   * @param \Ock\ClassDiscovery\Inspector\FactoryInspectorInterface $decorated
+   * @param iterable $candidates
+   *   Objects which may or may not contain decorator closures.
+   *
+   * @return \Ock\ClassDiscovery\Inspector\FactoryInspectorInterface
+   */
+  public static function applyDecorators(FactoryInspectorInterface $decorated, iterable $candidates): FactoryInspectorInterface {
+    foreach ($candidates as $candidate) {
+      if (!$candidate instanceof \Closure) {
+        continue;
+      }
+      $rf = new \ReflectionFunction($candidate);
+      $params = $rf->getParameters();
+      if (count($params) !== 1) {
+        continue;
+      }
+      $type = $params[0]->getType();
+      if (!$type) {
+        continue;
+      }
+      // See https://youtrack.jetbrains.com/issue/WI-77852/ReflectionType-toString-no-longer-deprecated
+      // @phpstan-ignore-next-line
+      if ($type->__toString() !== FactoryInspectorInterface::class) {
+        continue;
+      }
+      $decorator = $candidate($decorated);
+      if (!$decorator instanceof FactoryInspectorInterface) {
+        continue;
+      }
+      $decorated = $decorator;
+    }
+    return $decorated;
+  }
+
+}
