4be162e3f87d50a752b3311eb3a1c76180850752
Andreas Hennings <andreas@dqxtech.net>
2024-06-15 15:50:31 +0200

    Register methods as services if #[Service] attribute.

--- packages/dependency-injection/tests/src/Fixtures/ServiceAttribute/AbstractClass.php
+++ packages/dependency-injection/tests/src/Fixtures/ServiceAttribute/AbstractClass.php
@@ -4,6 +4,23 @@ declare(strict_types=1);
 
 namespace Ock\DependencyInjection\Tests\Fixtures\ServiceAttribute;
 
+use Ock\DependencyInjection\Attribute\Service;
+
 abstract class AbstractClass {
+
+  #[Service]
+  public static function createExample(): ClassWithoutAttribute {
+    return new ClassWithoutAttribute();
+  }
+
+  #[Service(target: 'exampleTarget')]
+  public static function createWithTarget(): ClassWithoutAttribute {
+    return new ClassWithoutAttribute();
+  }
+
+  #[Service(serviceIdSuffix: 'exampleSuffix')]
+  public static function createWithSuffix(): ClassWithoutAttribute {
+    return new ClassWithoutAttribute();
+  }
 
 }
