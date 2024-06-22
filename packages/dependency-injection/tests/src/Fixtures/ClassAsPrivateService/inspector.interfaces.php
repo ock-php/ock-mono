0af989b6ec37d8a7c3ff5c78af03d5d45a7bc86c
Andreas Hennings <andreas@dqxtech.net>
2024-06-22 15:23:39 +0200

    Remove ClassInspector_InterfaceAsFact.

--- packages/dependency-injection/tests/src/Fixtures/ClassAsPrivateService/inspector.interfaces.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-use Ock\ClassDiscovery\Inspector\PackageInspector;
-use Ock\DependencyInjection\Inspector\ClassInspector_InterfaceAsFact;
-
-return PackageInspector::fromCandidateObjects([
-  new ClassInspector_InterfaceAsFact(),
-]);
