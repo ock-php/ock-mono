4c16c38b4c8f356966fcf05bbab299b607dd9482
Andreas Hennings <andreas@dqxtech.net>
2024-07-19 14:35:35 +0200

    adaptism: Add ServicesTest::testServicesAsRecorded().

--- packages/adaptism/tests/src/ServicesTest.php
+++ packages/adaptism/tests/src/ServicesTest.php
@@ -4,40 +4,75 @@ declare(strict_types=1);
 
 namespace Ock\Adaptism\Tests;
 
-use Ock\Adaptism\AdapterDefinitionList\AdapterDefinitionListInterface;
-use Ock\Adaptism\AdaptismPackage;
-use Ock\Adaptism\Inspector\FactoryInspector_AdapterAttribute;
-use Ock\Adaptism\Inspector\FactoryInspector_SelfAdapterAttribute;
-use Ock\Adaptism\Tests\Fixtures\AdaptismTestNamespace;
 use Ock\Adaptism\Tests\Fixtures\FixturesUtil;
-use Ock\Adaptism\UniversalAdapter\UniversalAdapterInterface;
+use Ock\Testing\Exporter\Exporter_ToYamlArray;
+use Ock\Testing\RecordedTestTrait;
 use PHPUnit\Framework\TestCase;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Exception\ServiceNotFoundException;
 
 class ServicesTest extends TestCase {
 
-  /**
-   * Verifies that the common services are found in the test container.
-   */
-  public function testServicesExist(): void {
+  use RecordedTestTrait;
+
+  public function testServicesAsRecorded(): void {
     $container = FixturesUtil::getContainer();
-
-    $service = $container->get(UniversalAdapterInterface::class);
-    static::assertInstanceOf(UniversalAdapterInterface::class, $service);
-
-    $service = $container->get(AdapterDefinitionListInterface::class);
-    static::assertInstanceOf(AdapterDefinitionListInterface::class, $service);
+    $ids = $container->getServiceIds();
+    $report = [];
+    # $ids = \array_diff($ids, ['service_container']);
+    \sort($ids);
+    foreach ($ids as $id) {
+      if (!$container->has($id)) {
+        $report[$id] = '(not available)';
+        continue;
+      }
+      try {
+        $report[$id] = $container->get($id);
+      }
+      catch (ServiceNotFoundException $e) {
+        if ($e->getId() === $id) {
+          $report[$id] = '(not found)';
+        }
+        else {
+          $report[$id] = $e;
+        }
+      }
+      catch (\Throwable $e) {
+        $report[$id] = $e;
+      }
+    }
+    $this->assertObjectsAsRecorded(
+      $report,
+      arrayKeyIsDefaultClass: true,
+    );
   }
 
-  public function testTaggedServicesExist(): void {
+  /**
+   * Verifies tagged service ids.
+   */
+  public function testTaggedServicesAsRecorded(): void {
     $container = FixturesUtil::getContainer();
-    static::assertInstanceOf(ContainerBuilder::class, $container);
-    $taggedIds = $container->findTaggedServiceIds(AdaptismPackage::DISCOVERY_TAG_NAME);
-    static::assertSame([
-      FactoryInspector_AdapterAttribute::class => [[]],
-      FactoryInspector_SelfAdapterAttribute::class => [[]],
-      AdaptismTestNamespace::class => [[]],
-    ], $taggedIds);
+    $tag_names = $container->findTags();
+    $report = [];
+    foreach ($tag_names as $tag_name) {
+      foreach ($container->findTaggedServiceIds($tag_name) as $service_id => $tags_info) {
+        if ($tags_info === [[]]) {
+          $tags_info = '[[]]';
+        }
+        $report[$tag_name][$service_id] = $tags_info;
+      }
+    }
+    $this->assertAsRecorded($report, depth: 4);
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function createExporter(): Exporter_ToYamlArray {
+    return (new Exporter_ToYamlArray())
+      ->withDedicatedExporter(ContainerBuilder::class, fn (
+        ContainerBuilder $builder,
+      ) => ['class' => \get_class($builder)]);
   }
 
 }
