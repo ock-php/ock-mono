16595c4ce98a6bdea917a696548ab1a47d6c8aa9
Andreas Hennings <andreas@dqxtech.net>
2024-07-22 02:36:24 +0200

    modules/ock: Drop the #[PublicService] attribute and mechanism.

--- modules/ock/src/OckServiceProvider.php
+++ modules/ock/src/OckServiceProvider.php
@@ -4,12 +4,10 @@ declare(strict_types = 1);
 
 namespace Drupal\ock;
 
-use Drupal\ock\Attribute\DI\PublicService;
 use Drupal\service_discovery\ModuleServiceProviderBase;
 use Ock\Adaptism\AdaptismPackage;
 use Ock\Egg\EggPackage;
 use Ock\Ock\OckPackage;
-use Symfony\Component\DependencyInjection\ChildDefinition;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 
 /**
@@ -25,14 +23,6 @@ class OckServiceProvider extends ModuleServiceProviderBase {
     (new EggPackage())->register($container);
     (new AdaptismPackage())->register($container);
     (new OckPackage())->register($container);
-
-    // Make services public that have the
-    $container->registerAttributeForAutoconfiguration(
-      PublicService::class,
-      static function (ChildDefinition $definition, PublicService $attribute, \ReflectionClass $class_or_method): void {
-        $definition->setPublic(true);
-      },
-    );
 
     // Register packages in the module itself.
     parent::doRegister($container);
