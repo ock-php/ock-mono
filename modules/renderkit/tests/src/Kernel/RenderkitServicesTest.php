bd74a97b577ec1ee3bf6c98e75c8d110d882e9aa
Andreas Hennings <andreas@dqxtech.net>
2024-10-03 21:59:23 +0200

    Issue #40: Use new base class in RenderkitServicesTest.

--- modules/renderkit/tests/src/Kernel/RenderkitServicesTest.php
+++ modules/renderkit/tests/src/Kernel/RenderkitServicesTest.php
@@ -4,79 +4,16 @@ declare(strict_types=1);
 
 namespace Drupal\Tests\renderkit\Kernel;
 
-use Drupal\Core\DependencyInjection\ContainerBuilder;
-use Drupal\Core\Extension\ModuleInstallerInterface;
-use Drupal\KernelTests\KernelTestBase;
-use Ock\Testing\RecordedTestTrait;
+use Drupal\service_discovery\Testing\ServicesTestBase;
 
-class RenderkitServicesTest extends KernelTestBase {
-
-  use RecordedTestTrait;
+/**
+ * Test for services from renderkit module.
+ */
+class RenderkitServicesTest extends ServicesTestBase {
 
   /**
-   * Modules to enable.
-   *
-   * @var array
+   * Additional modules to install.
    */
-  protected static $modules = [
-    'user',
-    'service_discovery',
-    'ock',
-    'renderkit',
-  ];
-
-  /**
-   * {@inheritdoc}
-   */
-  protected function setUp(): void {
-    parent::setUp();
-
-    $this->installEntitySchema('user');
-    $this->installSchema('user', ['users_data']);
-  }
-
-  /**
-   * Tests service ids from this module.
-   *
-   * This protects against services being removed accidentally.
-   *
-   * @return list<string>
-   *   Service ids from this module.
-   */
-  public function testServiceIds(): array {
-    $container = \Drupal::getContainer();
-    $all_ids = $container->getServiceIds();
-    /** @var ModuleInstallerInterface $module_installer */
-    $module_installer = \Drupal::service(ModuleInstallerInterface::class);
-    $module_installer->uninstall(['renderkit']);
-    $remaining_ids = \Drupal::getContainer()->getServiceIds();
-    $module_service_ids = array_values(array_diff($all_ids, $remaining_ids));
-    sort($module_service_ids);
-    $this->assertAsRecorded($module_service_ids, 'Service ids from renderkit module.');
-    return $module_service_ids;
-  }
-
-  /**
-   * @param list<string> $service_ids
-   *
-   * @depends testServiceIds
-   */
-  public function testServicesExist(array $service_ids): void {
-    $container = \Drupal::getContainer();
-    $this->assertInstanceOf(ContainerBuilder::class, $container);
-    $types = [];
-    foreach ($service_ids as $id) {
-      $definition = $container->getDefinition($id);
-      if (!$definition->isPublic()) {
-        continue;
-      }
-      $service = \Drupal::service($id);
-      $types[$id] = get_debug_type($service);
-      if ($types[$id] === $id) {
-        $types[$id] = '=';
-      }
-    }
-    $this->assertAsRecorded($types, 'Service types.');
-  }
+  protected static $modules = ['ock'];
 
 }
