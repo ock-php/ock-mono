01f632d57bd31af4a928b9483bc460ffb28c71eb
Andreas Hennings <andreas@dqxtech.net>
2024-10-07 22:57:59 +0200

    Issue #46: Fix OckPresetServiceProvider base class.

--- modules/ock/modules/ock_preset/src/OckPresetServiceProvider.php
+++ modules/ock/modules/ock_preset/src/OckPresetServiceProvider.php
@@ -4,11 +4,11 @@ declare(strict_types = 1);
 
 namespace Drupal\ock_preset;
 
-use Drupal\ock\OckServiceProviderBase;
+use Drupal\service_discovery\ModuleServiceProviderBase;
 
 /**
  * Service provider for the ock_preset module.
  */
-class OckPresetServiceProvider extends OckServiceProviderBase {
+class OckPresetServiceProvider extends ModuleServiceProviderBase {
 
 }
