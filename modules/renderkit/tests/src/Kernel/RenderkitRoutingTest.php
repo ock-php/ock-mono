0f1d67bc8fe50b14ae668d2407ab98b712d6801e
Andreas Hennings <andreas@dqxtech.net>
2024-10-07 16:29:50 +0200

    Issue #41: Test routes from renderkit module.

---
+++ modules/renderkit/tests/src/Kernel/RenderkitRoutingTest.php
@@ -0,0 +1,20 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace Drupal\Tests\renderkit\Kernel;
+
+use Drupal\Tests\ock\Kernel\OckRoutingTestBase;
+
+class RenderkitRoutingTest extends OckRoutingTestBase {
+
+  /**
+   * Modules to enable.
+   *
+   * @var array
+   */
+  protected static $modules = [
+    'ock',
+  ];
+
+}
