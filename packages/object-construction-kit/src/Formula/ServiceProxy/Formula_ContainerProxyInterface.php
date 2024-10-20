b9d60af4c27cf72344f02af06e41819b4c4bf8db
Andreas Hennings <andreas@dqxtech.net>
2024-10-20 20:32:19 +0200

    Issue #52: Enhance docs in Formula_ContainerProxyInterface.

--- packages/object-construction-kit/src/Formula/ServiceProxy/Formula_ContainerProxyInterface.php
+++ packages/object-construction-kit/src/Formula/ServiceProxy/Formula_ContainerProxyInterface.php
@@ -8,16 +8,22 @@ use Ock\Adaptism\Attribute\SelfAdapter;
 use Ock\Ock\Core\Formula\FormulaInterface;
 use Psr\Container\ContainerInterface;
 
+/**
+ * Proxy formula that provides the real formula when given a service container.
+ */
 interface Formula_ContainerProxyInterface extends FormulaInterface {
 
   /**
    * Gets the "real" formula with resolved dependencies.
    *
    * @param \Psr\Container\ContainerInterface $container
+   *   Service container from which to get a formula.
    *
    * @return \Ock\Ock\Core\Formula\FormulaInterface
+   *   The formula.
    *
    * @throws \Ock\Ock\Exception\FormulaException
+   *   The formula cannot be created or found.
    *
    * @noinspection PhpAttributeCanBeAddedToOverriddenMemberInspection
    */
