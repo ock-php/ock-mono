0a44733fdf55e8732ecd7210e6a9deb6c796322c
Andreas Hennings <andreas@dqxtech.net>
2024-10-26 16:12:09 +0200

    Issue #46: Set a timeout for web driver requests, to avoid hanging tests.

--- modules/ock/tests/src/FunctionalJavascript/OckWebDriverTestBase.php
+++ modules/ock/tests/src/FunctionalJavascript/OckWebDriverTestBase.php
@@ -5,6 +5,8 @@ declare(strict_types = 1);
 namespace Drupal\Tests\ock\FunctionalJavascript;
 
 use Drupal\FunctionalJavascriptTests\WebDriverTestBase;
+use WebDriver\Service\CurlService;
+use WebDriver\ServiceFactory;
 
 /**
  * Enhanced base class for web driver tests.
@@ -15,5 +17,25 @@ class OckWebDriverTestBase extends WebDriverTestBase {
    * {@inheritdoc}
    */
   protected $defaultTheme = 'stark';
+
+  /**
+   * {@inheritdoc}
+   */
+  public function setUp(): void {
+    // Set a timeout for web driver requests.
+    // Otherwise, the test will hang if the web driver is unavailable.
+    ServiceFactory::getInstance()->setServiceClass(
+      'service.curl',
+      get_class(new class () extends CurlService {
+        public function __construct($defaultOptions = []) {
+          /** @noinspection PhpComposerExtensionStubsInspection */
+          $defaultOptions[CURLOPT_TIMEOUT] = 1;
+          parent::__construct($defaultOptions);
+        }
+      }),
+    );
+
+    parent::setUp();
+  }
 
 }
