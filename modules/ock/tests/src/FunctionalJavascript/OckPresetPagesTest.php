cd4991ff486b2a4ba89c1c769d4621f7f11cdc25
Andreas Hennings <andreas@dqxtech.net>
2024-10-27 02:20:51 +0100

    Issue #46: Test a preset used in another preset.

--- modules/ock/tests/src/FunctionalJavascript/OckPresetPagesTest.php
+++ modules/ock/tests/src/FunctionalJavascript/OckPresetPagesTest.php
@@ -4,6 +4,15 @@ declare(strict_types = 1);
 
 namespace Drupal\Tests\ock\FunctionalJavascript;
 
+use Drupal\ock_example\Animal\Animal_Elephant;
+use Drupal\ock_example\Plant\Plant_EnchantedCreature;
+use Drupal\ock_example\Plant\PlantInterface;
+use Drupal\ock_preset\Crud\PresetRepository;
+use Ock\Adaptism\UniversalAdapter\UniversalAdapterInterface;
+use Ock\Ock\Evaluator\Evaluator;
+use Ock\Ock\Generator\Generator;
+use Ock\Ock\Summarizer\Summarizer;
+use Ock\Ock\Translator\Translator;
 use WebDriver\Service\CurlService;
 use WebDriver\ServiceFactory;
 
@@ -101,6 +110,83 @@ class OckPresetPagesTest extends OckWebDriverTestBase {
 
     // Back on the list page, refreshed.
     $this->assertSession()->pageTextNotContains('Laura');
+  }
+
+  /**
+   * Tests using one preset as part of another preset.
+   */
+  public function testPresetAsPlugin(): void {
+    /** @var \Drupal\ock_preset\Crud\PresetRepository $repo */
+    $repo = \Drupal::service(PresetRepository::class);
+    /** @var \Ock\Adaptism\UniversalAdapter\UniversalAdapterInterface $adapter */
+    $adapter = \Drupal::service(UniversalAdapterInterface::class);
+
+    // Create an animal preset.
+    $this->drupalGet('/admin/structure/ock_preset/Drupal.ock_example.Animal.AnimalInterface/add');
+    $this->assertSession()->fieldExists('Administrative title')->setValue('Pink elephant');
+    $this->selectAndWait('Plugin', 'Elephant');
+    $this->assertSession()->buttonExists('Save preset')->click();
+
+    // Make sure the new preset option is available in the select.
+    $this->drupalGet('/admin/structure/ock_preset/Drupal.ock_example.Animal.AnimalInterface/add');
+    $this->selectAndWait('Plugin', 'Preset: Pink elephant');
+
+    // Use the new preset within another preset.
+    $this->drupalGet('/admin/structure/ock_preset/Drupal.ock_example.Plant.PlantInterface/add');
+    $this->assertSession()->fieldExists('Administrative title')->setValue('Enchanted pink elephant');
+    $this->selectAndWait('Plugin', 'Enchanted creature…');
+    $this->selectAndWait('Animal', 'Preset: Pink elephant');
+    $this->assertSession()->buttonExists('Save preset')->click();
+
+    $config = $repo->load(PlantInterface::class, 'enchanted_pink_elephant');
+    $this->assertFalse($config->isNew());
+    $this->assertSame(
+      [
+        'label' => 'Enchanted pink elephant',
+        'conf' => [
+          'id' => 'enchantedCreature',
+          'options' => [
+            'animal' => [
+              'id' => 'pink_elephant',
+            ],
+          ],
+        ],
+      ],
+      $config->getRawData(),
+    );
+
+    foreach ([
+      'preset id' => ['id' => 'enchanted_pink_elephant'],
+      'stored config' => $config->get('conf'),
+    ] as $name => $conf) {
+      $php = Generator::fromIface(PlantInterface::class, $adapter)
+        ->confGetPhp($conf);
+      $this->assertSame(
+        <<<PHP
+new \Drupal\ock_example\Plant\Plant_EnchantedCreature(
+new \Drupal\ock_example\Animal\Animal_Elephant(),
+)
+PHP,
+        $php,
+        "Generated php expression for $name",
+      );
+
+      $object = Evaluator::iface(PlantInterface::class, $adapter)
+        ->confGetValue($conf);
+      $this->assertInstanceOf(Plant_EnchantedCreature::class, $object);
+      $this->assertInstanceOf(Animal_Elephant::class, $object->enchanted_animal);
+
+      $summary = Summarizer::fromIface(PlantInterface::class, $adapter)
+        ->confGetSummary($conf);
+      $this->assertSame(
+        match ($name) {
+          'preset id' => 'Preset: Enchanted pink elephant',
+          'stored config' => 'Enchanted creature: <ul><li>Animal: Preset: Pink elephant</li></ul>',
+        },
+        $summary->convert(Translator::passthru()),
+        "Generated summary for $name",
+      );
+    }
   }
 
 }
