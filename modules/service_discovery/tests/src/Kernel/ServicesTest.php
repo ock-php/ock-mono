764ac7b312e79037074d903f41728969618e11a0
Andreas Hennings <andreas@dqxtech.net>
2024-11-02 00:45:46 +0100

    Issue #86: Fix the ServicesTest for service_discovery module.

--- modules/service_discovery/tests/src/Kernel/ServicesTest.php
+++ modules/service_discovery/tests/src/Kernel/ServicesTest.php
@@ -5,19 +5,7 @@ declare(strict_types=1);
 namespace Drupal\Tests\service_discovery\Kernel;
 
 use Drupal\service_discovery\Testing\ServicesTestBase;
-use Ock\Testing\RecordedTestTrait;
 
 class ServicesTest extends ServicesTestBase {
-
-  use RecordedTestTrait;
-
-  /**
-   * Modules to enable.
-   *
-   * @var array
-   */
-  protected static $modules = [
-    'service_discovery_test',
-  ];
 
 }
