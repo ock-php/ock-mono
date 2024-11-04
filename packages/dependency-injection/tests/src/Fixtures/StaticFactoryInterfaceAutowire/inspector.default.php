a39104c915ff47f00c47de9d7040e3aed5e1d7ed
Andreas Hennings <andreas@dqxtech.net>
2024-11-04 23:53:18 +0100

    Issue #98: Remove 'symfony/config' dependency from all packages.

--- packages/dependency-injection/tests/src/Fixtures/StaticFactoryInterfaceAutowire/inspector.default.php
+++ packages/dependency-injection/tests/src/Fixtures/StaticFactoryInterfaceAutowire/inspector.default.php
@@ -1,5 +1,12 @@
 <?php
 
+/**
+ * @file
+ * In this scenario, autowire does not work correctly, due to a bug in symfony.
+ *
+ * See the 'fixed' scenario where one more inspector is added to fix autowire.
+ */
+
 use Ock\DependencyInjection\Inspector\ClassInspector_ClassAsPrivateService;
 use Ock\DependencyInjection\Inspector\FactoryInspector_ServiceAttribute;
 
