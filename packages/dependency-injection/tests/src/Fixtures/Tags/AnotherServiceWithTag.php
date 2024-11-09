868f41e02195872c42a45d65d093952860610439
Andreas Hennings <andreas@dqxtech.net>
2024-11-09 20:44:11 +0100

    Issue #98: Make AnotherServiceWithTag::create() service not public.

--- packages/dependency-injection/tests/src/Fixtures/Tags/AnotherServiceWithTag.php
+++ packages/dependency-injection/tests/src/Fixtures/Tags/AnotherServiceWithTag.php
@@ -4,12 +4,12 @@ declare(strict_types=1);
 
 namespace Ock\DependencyInjection\Tests\Fixtures\Tags;
 
-use Ock\DependencyInjection\Attribute\Service;
+use Ock\DependencyInjection\Attribute\PrivateService;
 use Ock\DependencyInjection\Attribute\ServiceTag;
 
 class AnotherServiceWithTag {
 
-  #[Service]
+  #[PrivateService]
   #[ServiceTag('sunny')]
   public static function create(): self {
     return new self();
