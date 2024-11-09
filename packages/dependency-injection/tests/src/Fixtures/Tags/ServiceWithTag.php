cbabcedc84f31b283aaf8cfc09773de327f7ad14
Andreas Hennings <andreas@dqxtech.net>
2024-11-09 20:41:32 +0100

    Issue #98: Rely on implicit private service in ServiceWithTag fixture class.

--- packages/dependency-injection/tests/src/Fixtures/Tags/ServiceWithTag.php
+++ packages/dependency-injection/tests/src/Fixtures/Tags/ServiceWithTag.php
@@ -4,11 +4,9 @@ declare(strict_types=1);
 
 namespace Ock\DependencyInjection\Tests\Fixtures\Tags;
 
-use Ock\DependencyInjection\Attribute\Service;
 use Ock\DependencyInjection\Attribute\ServiceTag;
 
 #[ServiceTag('sunny')]
-#[Service]
 class ServiceWithTag {
 
 }
