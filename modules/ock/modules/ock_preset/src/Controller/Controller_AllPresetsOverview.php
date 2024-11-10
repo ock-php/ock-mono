1dcfe89eaee64924f88d7aae9e330500dd514983
Andreas Hennings <andreas@dqxtech.net>
2024-11-10 20:21:30 +0100

    Issue 39: Move the ClassRouteHelper* classes.

--- modules/ock/modules/ock_preset/src/Controller/Controller_AllPresetsOverview.php
+++ modules/ock/modules/ock_preset/src/Controller/Controller_AllPresetsOverview.php
@@ -8,12 +8,12 @@ use Drupal\controller_attributes\Attribute\RouteIsAdmin;
 use Drupal\controller_attributes\Attribute\RouteMenuLink;
 use Drupal\controller_attributes\Attribute\RouteRequirePermission;
 use Drupal\controller_attributes\Attribute\RouteTitle;
+use Drupal\controller_attributes\ClassRouteHelper;
+use Drupal\controller_attributes\ClassRouteHelperInterface;
 use Drupal\controller_attributes\Controller\ControllerRouteNameInterface;
 use Drupal\controller_attributes\Controller\ControllerRouteNameTrait;
 use Drupal\Core\Controller\ControllerBase;
 use Drupal\Core\Render\Markup;
-use Drupal\ock\UI\RouteHelper\ClassRouteHelper;
-use Drupal\ock\UI\RouteHelper\ClassRouteHelperInterface;
 use Drupal\ock\Util\StringUtil;
 use Drupal\ock_preset\Crud\PresetRepository;
 use Ock\DID\Attribute\Parameter\GetService;
@@ -43,7 +43,7 @@ class Controller_AllPresetsOverview extends ControllerBase implements Controller
    * @param string $methodName
    *   Name of a method in this class.
    *
-   * @return \Drupal\ock\UI\RouteHelper\ClassRouteHelperInterface
+   * @return \Drupal\controller_attributes\ClassRouteHelperInterface
    *   Builder object to create links and urls.
    */
   public static function route(string $methodName = 'index'): ClassRouteHelperInterface {
