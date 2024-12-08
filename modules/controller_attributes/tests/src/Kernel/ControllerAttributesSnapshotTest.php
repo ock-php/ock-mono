8a52b2c279c0f6fdca27073b5460a09b153fe9cc
Andreas Hennings <andreas@dqxtech.net>
2024-12-08 20:44:52 +0100

    Issue 39: Add ControllerAttributesSnapshotTest, nothing to report for now.

---
+++ modules/controller_attributes/tests/src/Kernel/ControllerAttributesSnapshotTest.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace Drupal\Tests\controller_attributes\Kernel;
+
+use Ock\DrupalTesting\ModuleSnapshotTestBase;
+
+class ControllerAttributesSnapshotTest extends ModuleSnapshotTestBase {
+
+}
