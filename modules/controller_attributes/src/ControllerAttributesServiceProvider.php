dbf1be1f4c4782258ccd3cd4c1cc8388199739a2
Andreas Hennings <andreas@dqxtech.net>
2024-12-08 20:17:40 +0100

    Issue 39: Add ControllerAttributesServiceProvider, which does nothing for now.

---
+++ modules/controller_attributes/src/ControllerAttributesServiceProvider.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace Drupal\controller_attributes;
+
+use Drupal\service_discovery\ModuleServiceProviderBase;
+
+/**
+ * Service provider for this module.
+ */
+class ControllerAttributesServiceProvider extends ModuleServiceProviderBase {
+
+}
