9d3d375ebd45567cba22e7aafe59d18095e957bd
Andreas Hennings <andreas@dqxtech.net>
2025-04-20 01:20:44 +0200

    Issue 134: Introduce RealpathRootThisTrait.

---
+++ packages/class-discovery/src/ClassFilesIA/RealpathRootThisTrait.php
@@ -0,0 +1,19 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace Ock\ClassDiscovery\ClassFilesIA;
+
+/**
+ * @internal
+ */
+trait RealpathRootThisTrait {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function withRealpathRoot(): static {
+    return $this;
+  }
+
+}
