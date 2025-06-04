a9235ef04236caa091bc65bb51dfd5274f9f753b
Andreas Hennings <andreas@dqxtech.net>
2025-06-04 15:46:20 +0200

    Don't use service_discovery in LinkPluginDeriverBase.

--- modules/controller_attributes/src/PluginDeriver/LinkPluginDeriverBase.php
+++ modules/controller_attributes/src/PluginDeriver/LinkPluginDeriverBase.php
@@ -5,43 +5,27 @@ namespace Drupal\controller_attributes\PluginDeriver;
 
 use Drupal\Core\Plugin\Discovery\ContainerDeriverInterface;
 use Drupal\Core\Routing\RouteProviderInterface;
-use Drupal\service_discovery\CallbackResolverInterface;
 use Psr\Container\ContainerInterface;
-use Symfony\Component\DependencyInjection\Attribute\Autowire;
 use Symfony\Component\Routing\Route;
 use Symfony\Component\Routing\RouterInterface;
 
 abstract class LinkPluginDeriverBase extends PluginDeriverBase implements ContainerDeriverInterface {
 
-  /**
-   * Static factory.
-   *
-   * @param \Psr\Container\ContainerInterface $container
-   * @param string $base_plugin_id
-   *
-   * @return static
-   *
-   * @throws \Psr\Container\ContainerExceptionInterface
-   *   Some arguments are left unresolved.
-   */
-  public static function create(ContainerInterface $container, $base_plugin_id): static {
-    $resolver = $container->get(CallbackResolverInterface::class);
-    assert($resolver instanceof CallbackResolverInterface);
-    return $resolver
-      ->withTypeArgs(['string' => $base_plugin_id])
-      ->construct(static::class);
-  }
-
-  /**
-   * @param \Drupal\Core\Routing\RouteProviderInterface $provider
-   * @param \Symfony\Component\Routing\RouterInterface $router
-   */
   public function __construct(
-    #[Autowire(service: 'router.route_provider')]
     protected RouteProviderInterface $provider,
-    #[Autowire(service: 'router.no_access_checks')]
     private readonly RouterInterface $router,
   ) {}
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container, $base_plugin_id): static {
+    return new static(
+      $container->get(RouteProviderInterface::class),
+      $container->get('router.no_access_checks'),
+    );
+  }
+
 
   /**
    * @param \Symfony\Component\Routing\Route $route
