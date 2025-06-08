397f0486df932395ce737420baf90b9e50bbac45
Andreas Hennings <andreas@dqxtech.net>
2025-06-08 17:44:03 +0200

    Add defaults in Snapshotter_ActionLinks.

--- packages/drupal-testing/src/Snapshotter/Snapshotter_ActionLinks.php
+++ packages/drupal-testing/src/Snapshotter/Snapshotter_ActionLinks.php
@@ -4,6 +4,7 @@ declare(strict_types = 1);
 
 namespace Ock\DrupalTesting\Snapshotter;
 
+use Drupal\Core\Menu\LocalActionDefault;
 use Drupal\Core\Menu\LocalActionManagerInterface;
 use Ock\DrupalTesting\DrupalTesting;
 
@@ -15,6 +16,19 @@ class Snapshotter_ActionLinks extends SnapshotterBase {
   protected function getItems(): array {
     return DrupalTesting::service(LocalActionManagerInterface::class)
       ->getDefinitions();
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function getDefaultItem(): array {
+    return [
+      'id' => null,
+      'route_parameters' => [],
+      'weight' => null,
+      'options' => [],
+      'class' => LocalActionDefault::class,
+    ];
   }
 
 }
