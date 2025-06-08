a6437ed3c0d3bdb3ee18579949e2cbe030938ff2
Andreas Hennings <andreas@dqxtech.net>
2025-06-08 18:47:09 +0200

    Add more test fixtures with route attributes.

--- modules/controller_attributes/tests/modules/controller_attributes_test/src/Controller/HelloController.php
+++ modules/controller_attributes/tests/modules/controller_attributes_test/src/Controller/HelloController.php
@@ -5,15 +5,34 @@ declare(strict_types = 1);
 namespace Drupal\controller_attributes_test\Controller;
 
 use Drupal\controller_attributes\Attribute\Route;
+use Drupal\controller_attributes\Attribute\RouteMenuLink;
 
 #[Route('/controller-attributes-test')]
 class HelloController {
 
+  public function methodWithoutAttributes(): array {
+    return [];
+  }
+
+  #[RouteMenuLink]
   #[Route('/hello')]
   public function hello(): array {
     return [
       '#markup' =>  'Hello',
+      // Prevent a method from being seen as unused.
+      'sub_element' => $this->helloPrivate(),
     ];
+  }
+
+  #[RouteMenuLink('Goodbye', 'A farewell greeting')]
+  #[Route('/goodbye')]
+  public function goodbye(): array {
+    return [];
+  }
+
+  #[Route('/non-public-methods-are-ignored')]
+  private function helloPrivate(): array {
+    return [];
   }
 
 }
