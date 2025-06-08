22e587d8efd631590d32d00c7e29ae0bb0de5b44
Andreas Hennings <andreas@dqxtech.net>
2025-06-08 17:43:33 +0200

    Fix Snapshotter_TaskLinks.

--- packages/drupal-testing/src/Snapshotter/Snapshotter_TaskLinks.php
+++ packages/drupal-testing/src/Snapshotter/Snapshotter_TaskLinks.php
@@ -4,8 +4,8 @@ declare(strict_types = 1);
 
 namespace Ock\DrupalTesting\Snapshotter;
 
-use Drupal\Core\Menu\LocalActionManagerInterface;
 use Drupal\Core\Menu\LocalTaskDefault;
+use Drupal\Core\Menu\LocalTaskManagerInterface;
 use Ock\DrupalTesting\DrupalTesting;
 
 class Snapshotter_TaskLinks extends SnapshotterBase {
@@ -14,7 +14,7 @@ class Snapshotter_TaskLinks extends SnapshotterBase {
    * {@inheritdoc}
    */
   protected function getItems(): array {
-    return DrupalTesting::service(LocalActionManagerInterface::class)
+    return DrupalTesting::service(LocalTaskManagerInterface::class)
       ->getDefinitions();
   }
 
@@ -23,13 +23,13 @@ class Snapshotter_TaskLinks extends SnapshotterBase {
    */
   protected function getDefaultItem(): array {
     return [
-      'route_parameters' => [],
-      'parent_id' => NULL,
-      'weight' => NULL,
-      'options' => [],
-      'base_route' => null,
-      'class' => LocalTaskDefault::class,
       'id' => '',
+      'weight' => null,
+      'route_parameters' => [],
+      'base_route' => null,
+      'parent_id' => null,
+      'options' => [],
+      'class' => LocalTaskDefault::class,
     ];
   }
 
