0a2bd37d5543e9062b5029c7a3194dcab4d05b2c
Andreas Hennings <andreas@dqxtech.net>
2025-06-15 23:48:34 +0200

    More fixture classes.

--- modules/controller_attributes/tests/modules/controller_attributes_test/src/Controller/HelloController.php
+++ modules/controller_attributes/tests/modules/controller_attributes_test/src/Controller/HelloController.php
@@ -5,12 +5,38 @@ declare(strict_types = 1);
 namespace Drupal\controller_attributes_test\Controller;
 
 use Drupal\controller_attributes\Attribute\Route;
+use Drupal\controller_attributes\Attribute\RouteDefaults;
+use Drupal\controller_attributes\Attribute\RouteHttpMethod;
+use Drupal\controller_attributes\Attribute\RouteIsAdmin;
 use Drupal\controller_attributes\Attribute\RouteMenuLink;
+use Drupal\controller_attributes\Attribute\RouteOptions;
+use Drupal\controller_attributes\Attribute\RouteParameters;
+use Drupal\controller_attributes\Attribute\RouteRequirements;
 
 #[Route('/controller-attributes-test')]
 class HelloController {
 
   public function methodWithoutAttributes(): array {
+    return [];
+  }
+
+  /**
+   * This route will be ignored.
+   */
+  #[Route('/not-installed')]
+  #[RouteRequirements(['_module_dependencies' => 'not_installed'])]
+  public function notInstalled(): array {
+    return [];
+  }
+
+  #[Route('/post-or-put/{arg}')]
+  #[RouteHttpMethod('POST', 'PUT')]
+  #[RouteIsAdmin]
+  #[RouteDefaults(['x' => 'X'])]
+  #[RouteOptions(['a' => 'B'])]
+  #[RouteRequirements(['_module_dependencies' => 'user'])]
+  #[RouteParameters(['arg' => ['type' => 'entity:user']])]
+  public function postOrPut(): array {
     return [];
   }
 
